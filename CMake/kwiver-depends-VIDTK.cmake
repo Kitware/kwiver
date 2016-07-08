a615e2466c24befe5c59a0ff96f5b17468e06b12
Jacob Becker <jacob.becker@kitware.com>
2016-07-08 13:19:31 -0400

    add missing file

---
+++ CMake/kwiver-depends-VIDTK.cmake
@@ -0,0 +1,12 @@
+# Optional find and confgure VXL dependency
+
+option( KWIVER_ENABLE_VIDTK
+  "Enable VIDTK dependent code and plugins"
+  OFF
+  )
+
+if( KWIVER_ENABLE_VIDTK )
+  find_package( vidtk REQUIRED )
+  include_directories( SYSTEM ${VIDTK_INCLUDE_DIRS} )
+  link_directories( ${VIDTK_LIBRARY_DIR} )
+endif( KWIVER_ENABLE_VIDTK )
