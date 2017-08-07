f361b73aac83f80c41e08ebcd041f634ce86ac0c
Matt Brown <matt.brown@kitware.com>
2017-08-06 21:57:44 -0400

    Functioning OCV motion detector arrow.

--- arrows/ocv/register_algorithms.cxx
+++ arrows/ocv/register_algorithms.cxx
@@ -66,6 +66,7 @@
 #include <arrows/ocv/match_features_bruteforce.h>
 #include <arrows/ocv/match_features_flannbased.h>
 #include <arrows/ocv/hough_circle_detector.h>
+#include <arrows/ocv/motion_detector_MOG2.h>
 #include <arrows/ocv/stabilize_video_KLT.h>
 #include <arrows/ocv/warp_image.h>
 
@@ -253,6 +254,14 @@ register_factories( kwiver::vital::plugin_loader& vpm )
   fact = vpm.ADD_ALGORITHM( "ocv_KLT", kwiver::arrows::ocv::stabilize_video_KLT );
   fact->add_attribute( kwiver::vital::plugin_factory::PLUGIN_DESCRIPTION,
                     "Stabilize video using OpenCV KLT tracker." )
+    .add_attribute( kwiver::vital::plugin_factory::PLUGIN_MODULE_NAME, module_name )
+    .add_attribute( kwiver::vital::plugin_factory::PLUGIN_VERSION, "1.0" )
+    .add_attribute( kwiver::vital::plugin_factory::PLUGIN_ORGANIZATION, "Kitware Inc." )
+    ;
+  
+  fact = vpm.ADD_ALGORITHM( "ocv_MOG2", kwiver::arrows::ocv::motion_detector_MOG2 );
+  fact->add_attribute( kwiver::vital::plugin_factory::PLUGIN_DESCRIPTION,
+                    "OpenCV Gaussian mixture-based background segmentation." )
     .add_attribute( kwiver::vital::plugin_factory::PLUGIN_MODULE_NAME, module_name )
     .add_attribute( kwiver::vital::plugin_factory::PLUGIN_VERSION, "1.0" )
     .add_attribute( kwiver::vital::plugin_factory::PLUGIN_ORGANIZATION, "Kitware Inc." )
