d6c4f2c7171eb5699b53f591195fcdcb04ed7932
Keith Fieldhouse <keith.fieldhouse@kitware.com>
2018-03-12 16:37:48 -0400

    Vital Algorithm to filter to a max_count of dets
    Defaults to top max_count by confidence, but has option to
    randomize:
    
    Pipeline Example:
    
    process detection_reader
      :: detected_object_input
    
      # name of the output file
      :file_name detections.csv
    
      # Name of the input algorithm. Options are csv, kw18.
      :reader:type   csv
    
    process max_count_filter
      ::detected_object_filter
      block filter
        type =  max_count_filter
    
        block max_count_filter
          max_count = 100
          randomize = true
        endblock
      endblock
    
    connect from detection_reader.detected_object_set
      to max_count_filter.detected_object_set
    
    process detection_writer
      :: detected_object_output
    
      # name of the output file
      :file_name  filtered_detections_out.csv
    
      # Name of the output algorithm. Options are csv, kw18.
      :writer:type   csv
    
    connect from max_count_filter.detected_object_set
      to detection_writer.detected_object_set

---
+++ arrows/core/max_count_filter.h
@@ -0,0 +1,83 @@
+/*ckwg +29
+ * Copyright 2016-2017 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef KWIVER_ARROWS_MAX_COUNT_FILTER_H_
+#define KWIVER_ARROWS_MAX_COUNT_FILTER_H_
+
+#include <vital/vital_config.h>
+#include <arrows/core/kwiver_algo_core_export.h>
+
+#include <vital/algo/algorithm.h>
+#include <vital/algo/detected_object_filter.h>
+#include <vital/types/image_container.h>
+
+#include <utility>
+#include <set>
+
+namespace kwiver {
+namespace arrows {
+namespace core {
+
+// ----------------------------------------------------------------
+/**
+ * @brief Filters detections up to a maximum count
+ *
+ * Returns a set of at most max_count items
+ *
+ * By default top max_count (sorted by confidence) items
+ * are returned.
+ *
+ * If randomize is true, N random elements are reutned
+ */
+
+class KWIVER_ALGO_CORE_EXPORT max_count_filter
+  : public vital::algorithm_impl<max_count_filter, vital::algo::detected_object_filter>
+{
+public:
+
+  max_count_filter();
+  virtual ~max_count_filter() VITAL_DEFAULT_DTOR
+
+  virtual vital::config_block_sptr get_configuration() const;
+
+  virtual void set_configuration( vital::config_block_sptr config );
+  virtual bool check_configuration( vital::config_block_sptr config ) const;
+
+  virtual vital::detected_object_set_sptr filter( const vital::detected_object_set_sptr input_set ) const;
+
+private:
+  bool m_randomize;
+  int m_max_count;
+};
+
+}}} //End namespace
+
+
+#endif // KWIVER_ARROWS_CLASS_PROBABILITY_FILTER_H_
