cd6b14ff177bbde9ec4dd7b80f11a42ff1ed6e19
Taylor Cook <taylor.cook@kitware.com>
2018-07-05 14:25:32 -0400

    added docker feature

---
+++ docker/dockersetup.sh
@@ -0,0 +1,23 @@
+# Kitware 2018:
+#
+# Setup and run script for KWIVER docker 1.2.0  
+# Script must run in /src/docker/ directory for pathing purposes
+# 
+# Script optionally sets up a mounted shared volume between host/docker 
+# 	*Permission may have to be granted to docker to create a folder in /Shared
+
+
+# Initial setup
+docker build -t kwiver:1.2.0 .
+
+# Start without shared volume (default)
+docker run -it kwiver:1.2.0 /bin/bash
+
+#	OR
+
+# Start with shared volume 
+# **This will create a shared folder on both host/docker in /Shared/KWIVER1.2.0
+#docker run -it -v /Shared/KWIVER1.2.0:/Shared/KWIVER1.2.0 kwiver:1.2.0 /bin/bash
+
+
+
