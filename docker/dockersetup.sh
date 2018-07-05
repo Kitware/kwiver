b14d76ed76d6bf14e24dd4d85137146f4194ae1f
cookt2 <tjstrikermc@gmail.com>
2018-07-05 17:03:32 -0400

    fixed pathing of created volume

--- docker/dockersetup.sh
+++ docker/dockersetup.sh
@@ -1,10 +1,9 @@
 # Kitware 2018:
 #
-# Setup and run script for KWIVER docker 1.2.0  
+# Setup and run script for KWIVER docker 1.2.0
 # Script must run in /src/docker/ directory for pathing purposes
-# 
-# Script optionally sets up a mounted shared volume between host/docker 
-# 	*Permission may have to be granted to docker to create a folder in /Shared
+#
+# Script optionally sets up a mounted shared volume between host/docker
 
 
 # Initial setup
@@ -15,9 +14,9 @@ docker run -it kwiver:1.2.0 /bin/bash
 
 #	OR
 
-# Start with shared volume 
-# **This will create a shared folder on both host/docker in /Shared/KWIVER1.2.0
-#docker run -it -v /Shared/KWIVER1.2.0:/Shared/KWIVER1.2.0 kwiver:1.2.0 /bin/bash
+# Start with shared volume
+# **This will create a shared folder on both host/docker in /SharedKIWVER/KWIVER1.2.0
+#docker run -it -v /SharedKWIVER/KWIVER1.2.0:/SharedKWIVER/KWIVER1.2.0 kwiver:1.2.0 /bin/bash
 
 
 
