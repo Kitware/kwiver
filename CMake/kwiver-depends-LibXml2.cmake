172d72460f671dc14f6dcda073e5b10c9a133786
Mmanu Chaturvedi <mmanu.chaturvedi@kitware.com>
2019-05-03 16:44:25 -0400

    Add LibXml2 as a dependency to GDAL

--- CMake/kwiver-depends-LibXml2.cmake
+++ CMake/kwiver-depends-LibXml2.cmake
@@ -1,7 +1,7 @@
 # if MIE4NITF is enabled, we need to have LibXml2 as well.
 
-if (KWIVER_ENABLE_MIE4NITF)
+if (KWIVER_ENABLE_GDAL)
   # Optionally find and configure LibXml2 dependency
   find_package( LibXml2 REQUIRED )
   include_directories(SYSTEM ${LIBXML2_INCLUDE_DIR})
-endif (KWIVER_ENABLE_MIE4NITF)
+endif (KWIVER_ENABLE_GDAL)
