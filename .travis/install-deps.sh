bb4735d63551fd4b8368ac61ce5d4df70a6c70de
David Stoup <david.stoup@kitware.com>
2019-07-25 08:16:56 -0400

    Update to bionic

--- .travis/install-deps.sh
+++ .travis/install-deps.sh
@@ -12,15 +12,6 @@ mkdir -p $INSTALL_DIR
 # Make a directory to test installation of KWIVER into
 mkdir -p $HOME/install
 
-# check if directory is cached
-if [ ! -f "$INSTALL_DIR/bin/cmake" ]; then
-  cd /tmp
-  wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.sh
-  bash cmake-3.4.0-Linux-x86_64.sh --skip-license --prefix="$INSTALL_DIR/"
-else
-  echo 'Using cached CMake directory.';
-fi
-
 
 # download and unpack Fletch
 HASH_FILE="$HASH_DIR/fletch.sha512"
