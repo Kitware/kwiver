b21ec38184a4669368517f50b0563cc08ccb1bd3
Ameya Shringi <ameya.shringi@kitware.com>
2019-08-21 11:00:03 -0400

    Minmal setup.py to build image bindings

---
+++ vital/bindings/python/setup.py
@@ -0,0 +1,36 @@
+from skbuild import setup
+from setuptools import find_packages
+import os.path as osp
+import os
+
+kwiver_root = '../../../'
+kwiver_install_dir = 'kwiver'
+fletch_root = osp.join('/mnt','usb1', 'ashringi', 'pypi', 'fletch', 'build', 'release')
+setup(
+        name='kwiver',
+        version='1.4.0',
+        author='Kitware',
+        author_email='http://public.kitware.com/mailman/listinfo/kwiver-users',
+        cmake_install_dir=kwiver_install_dir,
+        cmake_source_dir=kwiver_root,
+        cmake_minimum_required_version='3.3',
+        packages=[ 'kwiver', 'kwiver.vital', 'kwiver.vital.types' ],
+        setup_requires=[
+                        'cmake',
+                        'scikit-build'
+                       ],
+        install_requires=[
+                          'numpy',
+                          'pillow',
+                          'six',
+                         ],
+        cmake_args=[
+                    '-DCMAKE_BUILD_TYPE=Release',
+                    '-DKWIVER_ENABLE_C_BINDINGS=ON',
+                    '-DKWIVER_ENABLE_PYTHON=ON',
+                    '-DKWIVER_PYTHON_MAJOR_VERSION=3',
+                    '-DPYBIND11_PYTHON_VERSION=3',
+                    '-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON',
+                    '-Dfletch_DIR={0}'.format(fletch_root)
+                   ],
+    )
