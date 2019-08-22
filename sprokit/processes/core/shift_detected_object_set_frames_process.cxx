33e84e7644cb17a47e6c1bc3a20fc4c2e3b34e99
David Joy <david.joy@krs-3004.khq.kitware.com>
2019-08-22 15:06:46 -0400

    Initial implementation of frame offset process for detected object set

---
+++ sprokit/processes/core/shift_detected_object_set_frames_process.cxx
@@ -0,0 +1,163 @@
+/*ckwg +29
+ * Copyright 2011-2018 by Kitware, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither name of Kitware, Inc. nor the names of any contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "shift_detected_object_set_frames_process.h"
+
+#include <vital/config/config_block.h>
+#include <sprokit/processes/kwiver_type_traits.h>
+#include <sprokit/pipeline/process_exception.h>
+
+/**
+ * \file shift_detected_object_set_frames_process.cxx
+ *
+ * \brief Implementation of the detected object set frame shift process
+ */
+
+namespace kwiver
+{
+
+class shift_detected_object_set_frames_process::priv
+{
+  public:
+    typedef int32_t number_t;
+    priv(number_t offset);
+    ~priv();
+
+    number_t remaining_offset;
+
+    static vital::config_block_key_t const config_value;
+    static vital::config_block_value_t const default_value;
+
+    static vital::detected_object_set_sptr const empty_detected_object_set_sptr;
+};
+
+vital::config_block_key_t const
+shift_detected_object_set_frames_process::priv::config_value =\
+  vital::config_block_key_t("offset");
+vital::config_block_value_t const
+shift_detected_object_set_frames_process::priv::default_value =\
+  vital::config_block_value_t("0");
+
+vital::detected_object_set_sptr const
+shift_detected_object_set_frames_process::priv::empty_detected_object_set_sptr =\
+  std::make_shared<vital::detected_object_set>();
+
+
+shift_detected_object_set_frames_process
+::shift_detected_object_set_frames_process(vital::config_block_sptr const& config)
+  : process(config)
+  , d()
+{
+  make_ports();
+  make_config();
+}
+
+shift_detected_object_set_frames_process
+::~shift_detected_object_set_frames_process()
+{
+}
+
+void
+shift_detected_object_set_frames_process
+::make_config()
+{
+  declare_configuration_key(
+    priv::config_value,
+    priv::default_value,
+    vital::config_block_description_t("The offset to shift the input by."));
+}
+
+void
+shift_detected_object_set_frames_process
+::make_ports()
+{
+  sprokit::process::port_flags_t required;
+
+  required.insert( flag_required );
+
+  // -- input --
+  declare_input_port_using_trait( detected_object_set, required );
+
+  // -- output --
+  declare_output_port_using_trait( detected_object_set, required );
+}
+
+void
+shift_detected_object_set_frames_process
+::_configure()
+{
+  // Configure the process.
+  {
+    priv::number_t offset = config_value<priv::number_t>(priv::config_value);
+
+    d.reset(new priv(offset));
+  }
+
+  process::_configure();
+}
+
+void
+shift_detected_object_set_frames_process
+::_step()
+{
+  while (d->remaining_offset != 0)
+  {
+    // Source or sink data until the remaining offset is 0.
+    if (d->remaining_offset > 0)
+    {
+      push_to_port_using_trait( detected_object_set,
+				d->empty_detected_object_set_sptr);
+      --d->remaining_offset;
+    }
+    else if (d->remaining_offset < 0)
+    {
+      (void)grab_from_port_using_trait( detected_object_set );
+      ++d->remaining_offset;
+    }
+  }
+
+  push_to_port_using_trait( detected_object_set,
+			    grab_from_port_using_trait( detected_object_set ) );
+
+  process::_step();
+}
+
+shift_detected_object_set_frames_process::priv
+::priv(number_t offset)
+  : remaining_offset(offset)
+{
+}
+
+shift_detected_object_set_frames_process::priv
+::~priv()
+{
+}
+
+}
