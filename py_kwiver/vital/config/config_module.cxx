7b83b1008c06666fff221fb70a982bef212eadf9
Ameya Shringi <ameya.shringi@kitware.com>
2019-10-02 17:36:50 -0400

    Fix includes for vital python config

--- py_kwiver/vital/config/config_module.cxx
+++ py_kwiver/vital/config/config_module.cxx
@@ -28,7 +28,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <vital/bindings/python/vital/config/config.h>
+#include <py_kwiver/vital/config/config.h>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
