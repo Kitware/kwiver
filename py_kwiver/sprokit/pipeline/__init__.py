b70eb5afdfeb2ff18fe22bcc3c916be77e80eb70
Ameya Shringi <ameya.shringi@kitware.com>
2019-10-07 17:16:55 -0400

    Fix config dependency
    both process and scheduler factory requires config bindings to be
    registered it they are not then it would result in bindings causing
    problem. Thus config import is added before process_factory or
    scheduler_factory is included

---
+++ py_kwiver/sprokit/pipeline/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from .datum import *
+from .edge import *
+from .pipeline import *
+from .process import *
+from .scheduler import *
+from .stamp import *
+from .utils import *
+from .version import *
+from kwiver.vital.config import *
+from .process_factory import *
+from .scheduler_factory import *
