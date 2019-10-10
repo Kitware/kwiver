0b4015d039631e88faadbcdeb5d68bbf4944870c
Ameya Shringi <ameya.shringi@kitware.com>
2019-10-10 14:39:01 -0400

    Use entrypoint during python registration

--- python/kwiver/vital/modules/registration.cxx
+++ python/kwiver/vital/modules/registration.cxx
@@ -39,12 +39,14 @@
 #pragma warning (pop)
 #endif
 
+#include <pybind11/stl.h>
 #include <python/kwiver/vital/util/pybind11.h>
 #include <python/kwiver/vital/util/python_exceptions.h>
 #include <python/kwiver/vital/util/python.h>
 
 #include <vital/plugin_loader/plugin_loader.h>
 #include <kwiversys/SystemTools.hxx>
+#include <string>
 
 #ifdef VITAL_LOAD_PYLIB_SYM
   #include <dlfcn.h>
@@ -64,11 +66,11 @@
 
 namespace py = pybind11;
 
-static void load();
+static void load_python_modules();
 static bool is_suppressed();
-static void _load_python_library_symbols();
-
-
+static void _load_python_library_symbols(const std::string python_library_path);
+static std::string _find_python_library();
+static void load_additional_cpp_modules(kwiver::vital::plugin_loader& vpm);
 // ==================================================================
 /**
  * @brief Python module loader.
@@ -76,8 +78,9 @@ static void _load_python_library_symbols();
  * This function is called by the plugin loader when it is scanning
  * all plugins. It looks like a standard registration entry point for
  * a set or processes, but it activates the python interpreter and
- * causes it to call vital.modules.module_loader.load_python_modules()
- *
+ * causes it to call vital.modules.module_loader.load_python_modules().
+ * Addtionally for the python package of kwiver it is used to register external
+ * c++ plugins by specifying a search paths for the plugins
  * Also note that setting the environment variable
  * VITAL_NO_PYTHON_MODULES will suppress loading all python modules.
  */
@@ -114,74 +117,118 @@ register_factories(kwiver::vital::plugin_loader& vpm)
     PyEval_SaveThread();
   }
 
-  _load_python_library_symbols();
+  std::string python_library_path = "";
+  {
+    kwiver::vital::python::gil_scoped_acquire acquire;
+    (void)acquire;
+    python_library_path = _find_python_library();
+  }
+  _load_python_library_symbols(python_library_path);
+
+  // Load python modules
+  {
+    kwiver::vital::python::gil_scoped_acquire acquire;
+    (void)acquire;
+    VITAL_PYTHON_IGNORE_EXCEPTION(load_python_modules())
+  }
 
   {
     kwiver::vital::python::gil_scoped_acquire acquire;
     (void)acquire;
-    VITAL_PYTHON_IGNORE_EXCEPTION(load())
+    VITAL_PYTHON_IGNORE_EXCEPTION(load_additional_cpp_modules(vpm))
   }
 }
-
 
 // ------------------------------------------------------------------
 /*
  * Uses environment variables and compiler definitions to determine where the
  * python shared library is and load its symbols.
  */
-void _load_python_library_symbols()
+void _load_python_library_symbols(const std::string python_library_path)
 {
   auto logger = kwiver::vital::get_logger("vital.python_modules");
-
-#ifdef VITAL_LOAD_PYLIB_SYM
-  const char *env_pylib = kwiversys::SystemTools::GetEnv( "PYTHON_LIBRARY" );
-
-  // cmake should provide this definition
-  #ifdef PYTHON_LIBRARY
-  const char *default_pylib = MACRO_STR_VALUE(PYTHON_LIBRARY);
-  #else
-  const char *default_pylib = NULL;
-  #endif
-
-  // First check if the PYTHON_LIBRARY environment variable is specified
-  if( env_pylib )
+  if (python_library_path.empty())
   {
-    LOG_DEBUG(logger, "Loading symbols from PYTHON_LIBRARY=" << env_pylib );
-    void* handle = dlopen( env_pylib, RTLD_LAZY | RTLD_GLOBAL );
-    if (!handle) {
-      LOG_ERROR(logger, "Cannot load library: " << dlerror());
+    #ifdef VITAL_LOAD_PYLIB_SYM
+    const char *env_pylib = kwiversys::SystemTools::GetEnv( "PYTHON_LIBRARY" );
+
+    // cmake should provide this definition
+    #ifdef PYTHON_LIBRARY
+    const char *default_pylib = MACRO_STR_VALUE(PYTHON_LIBRARY);
+    #else
+    const char *default_pylib = NULL;
+    #endif
+
+    // First check if the PYTHON_LIBRARY environment variable is specified
+    if( env_pylib )
+    {
+      LOG_DEBUG(logger, "Loading symbols from PYTHON_LIBRARY=" << env_pylib );
+      void* handle = dlopen( env_pylib, RTLD_LAZY | RTLD_GLOBAL );
+      if (!handle) {
+        LOG_ERROR(logger, "Cannot load library: " << dlerror());
+      }
     }
-  }
-  else if( default_pylib )
-  {
-    // If the PYTHON_LIBRARY environment variable is not specified, use the
-    // CMAKE definition of PYTHON_LIBRARY instead.
-    LOG_DEBUG(logger, "Loading symbols from default PYTHON_LIBRARY=" << default_pylib);
-    void* handle = dlopen( default_pylib, RTLD_LAZY | RTLD_GLOBAL );
-    if (!handle) {
-      LOG_ERROR(logger, "Cannot load library: " << dlerror());
+    else if( default_pylib )
+    {
+      // If the PYTHON_LIBRARY environment variable is not specified, use the
+      // CMAKE definition of PYTHON_LIBRARY instead.
+      LOG_DEBUG(logger, "Loading symbols from default PYTHON_LIBRARY=" << default_pylib);
+      void* handle = dlopen( default_pylib, RTLD_LAZY | RTLD_GLOBAL );
+      if (!handle) {
+        LOG_ERROR(logger, "Cannot load library: " << dlerror());
+      }
     }
+    else
+    {
+      LOG_DEBUG(logger, "Unable to pre-load python symbols because " <<
+                        "PYTHON_LIBRARY is undefined.");
+    }
+    #else
+      LOG_DEBUG(logger, "Not checking for python symbols");
+    #endif
   }
   else
   {
-    LOG_DEBUG(logger, "Unable to pre-load python symbols because " <<
-                      "PYTHON_LIBRARY is undefined.");
+    LOG_DEBUG(logger, "Loading symbols from PYTHON_LIBRARY=" << python_library_path.c_str() );
+    void *handle = dlopen( python_library_path.c_str(), RTLD_LAZY | RTLD_GLOBAL );
+    if (!handle) {
+      LOG_ERROR(logger, "Cannot load library: " << dlerror());
+    }
   }
-#else
-  LOG_DEBUG(logger, "Not checking for python symbols");
-#endif
 }
 
+std::string
+_find_python_library()
+{
+  py::object const module = py::module::import("kwiver.vital.util.find_python_library");
+  py::object const python_library_path = module.attr("find_python_library")();
+  return python_library_path.cast<std::string>();
+}
 
 // ------------------------------------------------------------------
 void
-load()
+load_python_modules()
 {
   py::object const modules = py::module::import("kwiver.vital.modules.module_loader");
   py::object const loader = modules.attr("load_python_modules");
   loader();
 }
 
+// -------------------------------------------------------------------
+void
+load_additional_cpp_modules(kwiver::vital::plugin_loader& vpm)
+{
+  auto logger = kwiver::vital::get_logger("vital.load_additional_cpp_paths");
+  py::object const modules =  py::module::import("kwiver.vital.util.entrypoint");
+  py::object const get_cpp_paths_from_entrypoint = modules.attr("get_cpp_paths_from_entrypoint");
+  py::object py_additional_paths = get_cpp_paths_from_entrypoint();
+  auto additional_paths = py_additional_paths.cast<std::vector<std::string>>();
+  for(auto& additional_path : additional_paths)
+  {
+    LOG_INFO( logger, "loading additional cpp plugins from" + additional_path );
+  }
+  vpm.load_plugins(additional_paths);
+}
 
 // ------------------------------------------------------------------
 bool
