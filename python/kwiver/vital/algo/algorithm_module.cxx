23d5f28585ac3651b17b865ab728c4ae6d7775f2
Ameya Shringi <ameya.shringi@kitware.com>
2020-01-14 08:54:21 -0500

    Add python bindings for detected object filter algorithm

--- python/kwiver/vital/algo/algorithm_module.cxx
+++ python/kwiver/vital/algo/algorithm_module.cxx
@@ -50,6 +50,7 @@
 #include <python/kwiver/vital/algo/trampoline/compute_ref_homography_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/compute_depth_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/convert_image_trampoline.txx>
+#include <python/kwiver/vital/algo/trampoline/detected_object_filter_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/image_object_detector_trampoline.txx>
 #include <python/kwiver/vital/algo/algorithm.h>
 #include <python/kwiver/vital/algo/analyze_tracks.h>
@@ -61,6 +62,7 @@
 #include <python/kwiver/vital/algo/compute_ref_homography.h>
 #include <python/kwiver/vital/algo/compute_stereo_depth_map.h>
 #include <python/kwiver/vital/algo/convert_image.h>
+#include <python/kwiver/vital/algo/detected_object_filter.h>
 #include <python/kwiver/vital/algo/image_object_detector.h>
 #include <sstream>
 
@@ -87,6 +89,8 @@ PYBIND11_MODULE(algorithm, m)
             algorithm_def_csdm_trampoline<>>(m, "compute_stereo_depth_map");
   register_algorithm<kwiver::vital::algo::convert_image,
             algorithm_def_ci_trampoline<>>(m, "convert_image");
+  register_algorithm<kwiver::vital::algo::detected_object_filter,
+            algorithm_def_dof_trampoline<>>(m, "detected_object_filter");
   register_algorithm<kwiver::vital::algo::image_object_detector,
             algorithm_def_iod_trampoline<>>(m, "image_object_detector");
 
@@ -100,4 +104,5 @@ PYBIND11_MODULE(algorithm, m)
   compute_ref_homography(m);
   compute_stereo_depth_map(m);
   convert_image(m);
+  detected_object_filter(m);
 }
