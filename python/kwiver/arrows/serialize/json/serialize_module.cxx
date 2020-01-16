131fff1fbe3aa676ac764046cd74d6d522e006c7
Ameya Shringi <ameya.shringi@kitware.com>
2020-01-16 09:33:44 -0500

    Add bindings to serialize detected object in json format

--- python/kwiver/arrows/serialize/json/serialize_module.cxx
+++ python/kwiver/arrows/serialize/json/serialize_module.cxx
@@ -1,10 +1,12 @@
 #include <pybind11/pybind11.h>
 
 #include <python/kwiver/arrows/serialize/json/serialize_bounding_box.h>
+#include <python/kwiver/arrows/serialize/json/serialize_detected_object.h>
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(json,  m)
 {
   serialize_bounding_box(m);
+  serialize_detected_object(m);
 }
