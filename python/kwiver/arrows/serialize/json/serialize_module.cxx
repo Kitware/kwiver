1d1d08be36cd6893af5ed8328d7d311ee3797729
Ameya Shringi <ameya.shringi@kitware.com>
2020-01-16 14:07:23 -0500

    Add bindings to serialize some vital types in JSON
    Includes bindings to serialize the following vital types in JSON:
    Detected object set
    Image
    Timestamp
    Track state
    Track
    Track set
    Object track state
    Object track set

--- python/kwiver/arrows/serialize/json/serialize_module.cxx
+++ python/kwiver/arrows/serialize/json/serialize_module.cxx
@@ -3,6 +3,14 @@
 #include <python/kwiver/arrows/serialize/json/serialize_bounding_box.h>
 #include <python/kwiver/arrows/serialize/json/serialize_detected_object.h>
 #include <python/kwiver/arrows/serialize/json/serialize_detected_object_type.h>
+#include <python/kwiver/arrows/serialize/json/serialize_detected_object_set.h>
+#include <python/kwiver/arrows/serialize/json/serialize_image.h>
+#include <python/kwiver/arrows/serialize/json/serialize_timestamp.h>
+#include <python/kwiver/arrows/serialize/json/serialize_track_state.h>
+#include <python/kwiver/arrows/serialize/json/serialize_track.h>
+#include <python/kwiver/arrows/serialize/json/serialize_track_set.h>
+#include <python/kwiver/arrows/serialize/json/serialize_object_track_state.h>
+#include <python/kwiver/arrows/serialize/json/serialize_object_track_set.h>
 
 namespace py = pybind11;
 
@@ -11,4 +19,12 @@ PYBIND11_MODULE(json,  m)
   serialize_bounding_box(m);
   serialize_detected_object(m);
   serialize_detected_object_type(m);
+  serialize_detected_object_set(m);
+  serialize_image(m);
+  serialize_timestamp(m);
+  serialize_track_state(m);
+  serialize_track(m);
+  serialize_track_set(m);
+  serialize_object_track_state(m);
+  serialize_object_track_set(m);
 }
