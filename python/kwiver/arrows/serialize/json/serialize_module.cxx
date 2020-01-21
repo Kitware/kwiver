3cfc66af49458ec64ad49b1fe4fd4cf3ef53d119
Ameya Shringi <ameya.shringi@kitware.com>
2020-01-21 10:06:31 -0500

    Add python bindings for activity and activity_type serialization

--- python/kwiver/arrows/serialize/json/serialize_module.cxx
+++ python/kwiver/arrows/serialize/json/serialize_module.cxx
@@ -1,5 +1,7 @@
 #include <pybind11/pybind11.h>
 
+#include <python/kwiver/arrows/serialize/json/serialize_activity.h>
+#include <python/kwiver/arrows/serialize/json/serialize_activity_type.h>
 #include <python/kwiver/arrows/serialize/json/serialize_bounding_box.h>
 #include <python/kwiver/arrows/serialize/json/serialize_detected_object.h>
 #include <python/kwiver/arrows/serialize/json/serialize_detected_object_type.h>
@@ -16,6 +18,8 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(json,  m)
 {
+  serialize_activity(m);
+  serialize_activity_type(m);
   serialize_bounding_box(m);
   serialize_detected_object(m);
   serialize_detected_object_type(m);
