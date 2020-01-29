b462ad9dade01b76f5a1bfaedbf9093ad76d4a4f
Ameya Shringi <ameya.shringi@kitware.com>
2020-01-29 13:57:17 -0500

    Add python bindings for integrate depth maps algorithm

--- python/kwiver/vital/algo/algorithm_module.cxx
+++ python/kwiver/vital/algo/algorithm_module.cxx
@@ -71,6 +71,7 @@
 #include <python/kwiver/vital/algo/trampoline/image_object_detector_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/initialize_cameras_landmarks_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/initialize_object_tracks_trampoline.txx>
+#include <python/kwiver/vital/algo/trampoline/integrate_depth_maps_trampoline.txx>
 #include <python/kwiver/vital/algo/algorithm.h>
 #include <python/kwiver/vital/algo/analyze_tracks.h>
 #include <python/kwiver/vital/algo/associate_detections_to_tracks.h>
@@ -102,6 +103,7 @@
 #include <python/kwiver/vital/algo/image_object_detector.h>
 #include <python/kwiver/vital/algo/initialize_cameras_landmarks.h>
 #include <python/kwiver/vital/algo/initialize_object_tracks.h>
+#include <python/kwiver/vital/algo/integrate_depth_maps.h>
 #include <sstream>
 
 namespace py = pybind11;
@@ -169,6 +171,8 @@ PYBIND11_MODULE(algorithm, m)
             algorithm_def_icl_trampoline<>>(m, "initialize_cameras_landmarks");
   register_algorithm<kwiver::vital::algo::initialize_object_tracks,
             algorithm_def_iot_trampoline<>>(m, "initialize_object_tracks");
+  register_algorithm<kwiver::vital::algo::integrate_depth_maps,
+            algorithm_def_idm_trampoline<>>(m, "integrate_depth_maps");
 
   analyze_tracks(m);
   associate_detections_to_tracks(m);
@@ -200,4 +204,5 @@ PYBIND11_MODULE(algorithm, m)
   image_object_detector(m);
   initialize_cameras_landmarks(m);
   initialize_object_tracks(m);
+  integrate_depth_maps(m);
 }
