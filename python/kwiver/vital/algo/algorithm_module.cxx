b39c6864799fe0020ff379c660fe8c3f5d7b43cb
Ameya Shringi <ameya.shringi@kitware.com>
2020-01-29 11:10:58 -0500

    Add python bindings for filter features algorithm

--- python/kwiver/vital/algo/algorithm_module.cxx
+++ python/kwiver/vital/algo/algorithm_module.cxx
@@ -65,6 +65,7 @@
 #include <python/kwiver/vital/algo/trampoline/estimate_similarity_transform_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/extract_descriptors_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/feature_descriptor_io_trampoline.txx>
+#include <python/kwiver/vital/algo/trampoline/filter_features_trampoline.txx>
 #include <python/kwiver/vital/algo/trampoline/image_object_detector_trampoline.txx>
 #include <python/kwiver/vital/algo/algorithm.h>
 #include <python/kwiver/vital/algo/analyze_tracks.h>
@@ -91,6 +92,7 @@
 #include <python/kwiver/vital/algo/estimate_similarity_transform.h>
 #include <python/kwiver/vital/algo/extract_descriptors.h>
 #include <python/kwiver/vital/algo/feature_descriptor_io.h>
+#include <python/kwiver/vital/algo/filter_features.h>
 #include <python/kwiver/vital/algo/image_object_detector.h>
 #include <sstream>
 
@@ -147,6 +149,8 @@ PYBIND11_MODULE(algorithm, m)
             algorithm_def_ed_trampoline<>>(m, "extract_descriptors");
   register_algorithm<kwiver::vital::algo::feature_descriptor_io,
             algorithm_def_fdio_trampoline<>>(m, "feature_descriptor_io");
+  register_algorithm<kwiver::vital::algo::filter_features,
+            algorithm_def_ff_trampoline<>>(m, "filter_features");
   register_algorithm<kwiver::vital::algo::image_object_detector,
             algorithm_def_iod_trampoline<>>(m, "image_object_detector");
 
@@ -175,4 +179,5 @@ PYBIND11_MODULE(algorithm, m)
   estimate_similarity_transform(m);
   extract_descriptors(m);
   feature_descriptor_io(m);
+  filter_features(m);
 }
