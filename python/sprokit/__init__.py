26f4f7e81396860d32316050e8dc130f27455f92
joncrall <erotemic@gmail.com>
2020-02-25 14:21:55 -0500

    Change DeprecationWarning to UserWarning to ensure visibility by default

--- python/sprokit/__init__.py
+++ python/sprokit/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.warn(
     '`import sprokit` is deprecated, use import kwiver.sprokit instead',
-    DeprecationWarning
+    UserWarning
 )
 
 from kwiver.sprokit import *  # NOQA
