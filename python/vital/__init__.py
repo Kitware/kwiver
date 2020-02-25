29cc3dae72986f8a15155de1bb895d808d039e3b
joncrall <erotemic@gmail.com>
2020-02-25 14:21:55 -0500

    Change DeprecationWarning to UserWarning to ensure visibility by default

--- python/vital/__init__.py
+++ python/vital/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.warn(
     '`import vital` is deprecated, use import kwiver.vital instead',
-    DeprecationWarning
+    UserWarning
 )
 
 from kwiver.vital import *  # NOQA
