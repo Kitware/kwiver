431b134a11c7a926dadce46abaf04c681cc4f263
Tom Osika <tommycollege558@gmail.com>
2020-09-02 07:59:12 -0400

    Further simplify

--- python/kwiver/sprokit/adapters/adapter_data_set.cxx
+++ python/kwiver/sprokit/adapters/adapter_data_set.cxx
@@ -43,70 +43,18 @@ namespace kwiver{
 namespace sprokit{
 namespace python{
 
-// Accepts a generic python object. If the object is either an integer or
-// a timestamp, we can convert it and add it to a port
-void add_value_correct_type(adapter_data_set &self, ::sprokit::process::port_t const& port, py::object obj)
+// Just add an int
+void add_dummy_val(adapter_data_set &self)
 {
-  if (py::isinstance<py::int_>(obj))
-  {
-    int casted_obj = obj.cast<int>();
-    self.add_value<int>(port, casted_obj);
-    return;
-  }
-
-  if (py::isinstance<vital::timestamp>(obj))
-  {
-    vital::timestamp casted_obj = obj.cast<vital::timestamp>();
-    self.add_value<vital::timestamp>(port, casted_obj);
-    return;
-  }
+  self.add_value<int>("int_port", 5);
 }
 
-// Take data of an unknown type from a port and return. If data is either an int or
-// a timestamp, we can convert it and return
-py::object get_port_data_correct_type(adapter_data_set &self, ::sprokit::process::port_t const& port)
+// Assume there's an int at 'int_port' and return it
+int get_port_data_dummy(adapter_data_set &self)
 {
-  kwiver::vital::any const any = self.get_port_data<kwiver::vital::any>(port);
-
-  if (typeid(int) == any.type())
-  {
-    return py::cast(kwiver::vital::any_cast<int>(any));
-  }
-
-  if (typeid(vital::timestamp) == any.type())
-  {
-    return py::cast(kwiver::vital::any_cast<vital::timestamp>(any));
-  }
-
-  std::string msg("Unable to convert object found at adapter data set port: ");
-  msg += port;
-  msg += ". Data is of type: ";
-  msg += any.type_name();
-  throw py::type_error(msg);
+  return self.get_port_data<int>("int_port");
 }
 
 }
 }
-}
-
-// Simplification of adapter_data_set. Can only add and get ints and timestamps
-// to and from various ports. Can think of adapter_data_set as an std::map
-PYBIND11_MODULE(adapter_data_set, m)
-{
-  py::enum_<adapter_data_set::data_set_type>(m, "DataSetType"
-    , "Type of data set.")
-    .value("data", adapter_data_set::data)
-    .value("end_of_input", adapter_data_set::end_of_input)
-  ;
-
-  py::class_< adapter_data_set, std::shared_ptr<adapter_data_set > > ads(m, "AdapterDataSet");
-    ads.def_static("create", &adapter_data_set::create
-        , (py::arg("type") = adapter_data_set::data_set_type::data))
-
-    // General add_value which can recognize ints and timestamps
-    .def("add_value", &kwiver::sprokit::python::add_value_correct_type)
-
-    // General get_value which can recognize ints and timestamps
-    .def("get_port_data", &kwiver::sprokit::python::get_port_data_correct_type)
-    ;
 }
