b1547a40b2808a791212cdc2594ee8aac9f669e3
David Stoup <david.stoup@kitware.com>
2020-10-28 15:08:50 -0400

    Support VTK v9. VTK interfaces have changed since v8

--- CMake/kwiver-depends-VTK.cmake
+++ CMake/kwiver-depends-VTK.cmake
@@ -6,16 +6,18 @@ option( KWIVER_ENABLE_VTK
   )
 
 if( KWIVER_ENABLE_VTK )
-    find_package(VTK REQUIRED
-        COMPONENTS
-        vtkCommonCore
-        vtkCommonDataModel
-        )
-    if(VTK_VERSION VERSION_LESS 8.2)
-        message(FATAL_ERROR "${PROJECT_NAME} supports VTK >= v8.2 "
-            "(Found ${VTK_VERSION})")
-    endif()
-
-  include(${VTK_USE_FILE})
-
+  find_package(VTK REQUIRED
+    COMPONENTS
+    vtkCommonCore
+    vtkCommonDataModel
+    )
+  if(VTK_VERSION VERSION_LESS 8.2)
+    message(FATAL_ERROR "${PROJECT_NAME} supports VTK >= v8.2 "
+      "(Found ${VTK_VERSION})")
+  elseif(NOT VTK_VERSION VERSION_LESS 8.90 )
+    get_target_property(VTK_INCLUDE_DIR VTK::CommonCore INTERFACE_INCLUDE_DIRECTORIES)
+    include_directories(SYSTEM "${VTK_INCLUDE_DIR}")
+  else()
+    include(${VTK_USE_FILE})
+  endif()
 endif( KWIVER_ENABLE_VTK )
