fd09d7d6cc9899ae7de3602c846736f7415ab90d
Eugene Borovikov <Eugene.Borovikov@kitware.com>
2021-03-23 14:51:23 -0400

    resection camera test stub

--- vital/algo/resection_camera.h
+++ vital/algo/resection_camera.h
@@ -10,15 +10,12 @@
 #ifndef VITAL_ALGO_RESECTION_CAMERA_H_
 #define VITAL_ALGO_RESECTION_CAMERA_H_
 
-#include <vital/vital_config.h>
+#include <vital/algo/algorithm.h>
+#include <vital/types/feature_track_set.h>
+#include <vital/types/camera_perspective.h>
+#include <vital/types/landmark_map.h>
 
 #include <vector>
-#include <memory>
-
-#include <vital/algo/algorithm.h>
-#include <vital/types/feature_set.h>
-#include <vital/types/match_set.h>
-#include <vital/types/camera_perspective.h>
 
 namespace kwiver {
 namespace vital {
@@ -31,35 +28,29 @@ class VITAL_ALGO_EXPORT resection_camera
   : public kwiver::vital::algorithm_def<resection_camera>
 {
 public:
-  /// Return the name of this algorithm
+  /// \return name of this algorithm
   static std::string static_type_name() { return "resection_camera"; }
 
-  /// Estimate camera parameters from 3D points and their corresponding projections
-  ///
-  /// \param [in]  pts2d 2d projections of pts3d in the same order as pts3d
-  /// \param [in]  pts3d 3d points in the same order as pts2d.  Both must be same size.
-  /// \param [in]  init_cal the initial guess intrinsic parameters of the camera
-  /// \param [out] inliers for each point, the value is true if
-  ///                      this pair is an inlier to the estimate
+  /// Estimate camera parameters from 3D points and their corresponding projections.
+  /// \return estimated camera parameters
   virtual
   kwiver::vital::camera_perspective_sptr
-  resection(std::vector<vector_2d> const& pts2d,
-            std::vector<vector_3d> const& pts3d,
-            kwiver::vital::camera_intrinsics_sptr init_cal = nullptr,
-            std::vector<bool>& inliers) const = 0;
+  resection(std::vector<vector_2d> const& pts2d, ///< [in]  2d projections of pts3d in the same order as pts3d
+            std::vector<vector_3d> const& pts3d, ///< [in]  3d points in the same order as pts2d, assuming a 1-1 correspondence
+            std::vector<bool>& inliers, ///< [out] inlier flags for each point, the value is true if this pair is an inlier to the estimate
+            kwiver::vital::camera_intrinsics_sptr init_cal = nullptr ///< [in]  initial guess intrinsic parameters of the camera
+  ) const = 0;
 
-  /// Estimate camera parameters for a frame from landmarks and tracks
-  ///
-  /// \param [in]  frame     frame number for which to estimate a camera
-  /// \param [in]  landmarks 3D landmarks locations to constrain camera
-  /// \param [in]  tracks    2D feature tracks in image coordinates
-  /// \param [in]  init_cal  the initial guess intrinsic parameters of the camera
+  /// Estimate camera parameters for a frame from landmarks and tracks.
+  /// This is a convenience function, constructing the pts2d and pts3d from a frame, landmarks, and tracks, and then call resection() with the point correspondences.
+  /// \return estimated camera parameters
   virtual
   kwiver::vital::camera_perspective_sptr
-  resection(kwiver::vital::frame_id_t const& frame,
-            kwiver::vital::landmark_map_sptr landmarks,
-            kwiver::vital::feature_track_set_sptr tracks,
-            kwiver::vital::camera_intrinsics_sptr init_cal = nullptr) const;
+  resection(kwiver::vital::frame_id_t const& frame, ///< [in]  frame number for which to estimate a camera
+            kwiver::vital::landmark_map_sptr landmarks, ///< [in]  3D landmarks locations to constrain camera
+            kwiver::vital::feature_track_set_sptr tracks, ///< [in]  2D feature tracks in image coordinates
+            kwiver::vital::camera_intrinsics_sptr init_cal = nullptr ///< [in]  initial guess intrinsic parameters of the camera
+  ) const;
 
 protected:
   resection_camera();
