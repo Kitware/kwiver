1b38645c45f49bd209ff7ca12594b61561c39eb3
Tom Osika <tommycollege558@gmail.com>
2021-04-21 18:01:53 -0400

    Allow pipeline includes in python, test

--- python/kwiver/sprokit/tests/sprokit/adapters/test-embedded_pipeline.py
+++ python/kwiver/sprokit/tests/sprokit/adapters/test-embedded_pipeline.py
@@ -30,6 +30,7 @@
 
 from kwiver.sprokit.util.test import find_tests, run_test, test_error
 
+
 def test_import(cpp_pipeline_dir, py_pipeline_dir):
     try:
         import kwiver.sprokit.adapters.embedded_pipeline
@@ -114,7 +115,6 @@ def test_api_calls(cpp_pipeline_dir, py_pipeline_dir):
         for (port, d) in ods:
             print("   port:", port, " value:", d.get_int())
 
-
     ep.wait()
 
     #######
@@ -127,8 +127,29 @@ def test_api_calls(cpp_pipeline_dir, py_pipeline_dir):
     ods = ep.receive()
     ep.stop()
 
+
+def _create_detected_object_set():
+    from kwiver.vital.types import (
+        DetectedObject,
+        DetectedObjectSet,
+        BoundingBoxD,
+    )
+
+    dos = DetectedObjectSet()
+    bbox = BoundingBoxD(0, 10, 100, 50)
+    dos.add(DetectedObject(bbox, 0.2))
+    dos.add(DetectedObject(bbox, 0.5))
+    dos.add(DetectedObject(bbox, 0.4))
+
+    return dos
+
+
 def run_roundtrip_pipeline(py_pipeline_dir, ads_in):
-    from kwiver.vital.types import DetectedObject, DetectedObjectSet, BoundingBoxD
+    from kwiver.vital.types import (
+        DetectedObject,
+        DetectedObjectSet,
+        BoundingBoxD,
+    )
     from kwiver.sprokit.adapters import adapter_data_set, embedded_pipeline
 
     pipeline_fname = "py_to_cpp_type_conversion.pipe"
@@ -151,22 +172,16 @@ def run_roundtrip_pipeline(py_pipeline_dir, ads_in):
 
     ep.wait()
 
-def _create_detected_object_set():
-    from kwiver.vital.types import DetectedObject, DetectedObjectSet, BoundingBoxD
-
-    dos = DetectedObjectSet()
-    bbox = BoundingBoxD(0, 10, 100, 50)
-    dos.add(DetectedObject(bbox, 0.2))
-    dos.add(DetectedObject(bbox, 0.5))
-    dos.add(DetectedObject(bbox, 0.4))
-
-    return dos
 
 # We need to check that objects added in a python process
 # are usable by raw C++ code. We'll use run_roundtrip_pipeline to make sure
 # C++ objects can be converted on both sides
 def test_cpp_conversion(cpp_pipeline_dir, py_pipeline_dir):
-    from kwiver.vital.types import DetectedObject, DetectedObjectSet, BoundingBoxD
+    from kwiver.vital.types import (
+        DetectedObject,
+        DetectedObjectSet,
+        BoundingBoxD,
+    )
     from kwiver.sprokit.adapters import adapter_data_set, embedded_pipeline
     from kwiver.sprokit.pipeline import datum
 
@@ -179,8 +194,44 @@ def test_cpp_conversion(cpp_pipeline_dir, py_pipeline_dir):
     # Create fresh objects. Make sure that this also works with adding datums directly
     ads_in = adapter_data_set.AdapterDataSet.create()
     ads_in["detected_object_set"] = datum.new(_create_detected_object_set())
-    print("Starting roundtrip pipeline with a datum containing a detected_object_set")
+    print(
+        "Starting roundtrip pipeline with a datum containing a detected_object_set"
+    )
     run_roundtrip_pipeline(py_pipeline_dir, ads_in)
+
+
+def test_including_pipe_files(cpp_pipeline_dir, py_pipeline_dir):
+    from kwiver.vital.types import (
+        DetectedObject,
+        DetectedObjectSet,
+        BoundingBoxD,
+    )
+    from kwiver.sprokit.adapters import adapter_data_set, embedded_pipeline
+
+    pipeline_fname = "test_includes.pipe"
+    path_to_pipe_file = os.path.join(py_pipeline_dir, pipeline_fname)
+
+    ep = embedded_pipeline.EmbeddedPipeline()
+
+    # Note that we include an extra parameter here. This is where
+    # to look for included pipe files
+    ep.build_pipeline(path_to_pipe_file, py_pipeline_dir)
+
+    ep.start()
+
+    ads_in = adapter_data_set.AdapterDataSet.create()
+    ads_in["port1"] = _create_detected_object_set()
+    print("Sending ads:", ads_in)
+    ep.send(ads_in)
+    ep.send_end_of_input()
+
+    while True:
+        ods = ep.receive()
+        if ods.is_end_of_data():
+            break
+        print("Recieved detected_object_set:", ods["port1"])
+
+    ep.wait()
 
 
 if __name__ == "__main__":
