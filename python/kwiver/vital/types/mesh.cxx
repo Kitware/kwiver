a3539c08d0c4445a1366cb52011139c7405dc5ea
Chet Nieter <chet.nieter@havnor.khq.kitware.com>
2021-05-10 15:09:55 -0400

    Exposed more of the mesh api and mesh io to python
    Exposed more of the mesh api and mesh io to python. This includes
    the accessor for the texture coordinates and the methods to write
    out the mesh to various formats

--- python/kwiver/vital/types/mesh.cxx
+++ python/kwiver/vital/types/mesh.cxx
@@ -6,6 +6,8 @@
 #include <vital/types/mesh.h>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
 
 namespace py = pybind11;
 
@@ -39,8 +41,48 @@ PYBIND11_MODULE(mesh, m)
       }
       return (unsigned int)0;
     })
+    .def("has_tex_coords", &kwiver::vital::mesh::has_tex_coords)
+    .def("tex_coords",   &kwiver::vital::mesh::tex_coords)
+    .def("set_tex_source",   &kwiver::vital::mesh::set_tex_source)
     .def_static("from_ply_file", [](std::string path)
-                                 {
-                                   return kwiver::vital::read_ply(path);
-                                 });
+    {
+      return kwiver::vital::read_ply(path);
+    })
+    .def_static("from_ply2_file", [](std::string path)
+    {
+      return kwiver::vital::read_ply2(path);
+    })
+    .def_static("from_obj_file", [](std::string path)
+    {
+      return kwiver::vital::read_obj(path);
+    })
+    .def_static("from_file", [](std::string path)
+    {
+      return kwiver::vital::read_mesh(path);
+    })
+    .def_static("to_ply2_file", [](std::string path, kwiver::vital::mesh& mesh)
+    {
+      return kwiver::vital::write_ply2(path, mesh);
+    })
+    .def_static("to_obj_file", [](std::string path, kwiver::vital::mesh& mesh)
+    {
+      return kwiver::vital::write_obj(path, mesh);
+    })
+    .def_static("to_kml_file", [](std::string path, kwiver::vital::mesh& mesh)
+    {
+      return kwiver::vital::write_kml(path, mesh);
+    })
+    .def_static("to_kml_collada_file", [](std::string path, kwiver::vital::mesh& mesh)
+    {
+      return kwiver::vital::write_kml_collada(path, mesh);
+    })
+    .def_static("to_vrml_file", [](std::string path, kwiver::vital::mesh& mesh)
+    {
+      return kwiver::vital::write_vrml(path, mesh);
+    });
+
+  py::enum_<kwiver::vital::mesh::tex_coord_type>(m, "tex_coord_type")
+    .value("TEX_COORD_NONE", kwiver::vital::mesh::tex_coord_type::TEX_COORD_NONE)
+    .value("TEX_COORD_ON_VERT", kwiver::vital::mesh::tex_coord_type::TEX_COORD_ON_VERT)
+    .value("TEX_COORD_ON_CORNER", kwiver::vital::mesh::tex_coord_type::TEX_COORD_ON_CORNER);
 }
