c81e25780dd33c19218c829d0ea4427bf21593ff
Eugene Borovikov <Eugene.Borovikov@kitware.com>
2021-05-14 18:13:22 -0400

    ceres-specific types back to ceres cam params

--- arrows/ceres/options.h
+++ arrows/ceres/options.h
@@ -55,6 +55,13 @@ public:
  */
 struct camera_options : public mvg::camera_options
 {
+  /// typedef for camera parameter map
+  typedef std::unordered_map< frame_id_t,
+                              std::vector< double > > cam_param_map_t;
+  typedef std::unordered_map< frame_id_t,
+                              unsigned int > cam_intrinsic_id_map_t;
+  typedef std::vector< std::pair< frame_id_t, double* > > frame_params_t;
+
   camera_options(): mvg::camera_options() {}
   camera_options( const camera_options& other );
   virtual void get_configuration( config_block_sptr config ) const override;
@@ -115,6 +122,31 @@ struct camera_options : public mvg::camera_options
                                   cam_param_map_t& ext_params,
                                   std::vector< std::vector< double > >& int_params,
                                   cam_intrinsic_id_map_t& int_map ) const;
+
+  /**
+   * Update the camera objects using the extracted camera parameters.
+   *
+   *  \param [out] cameras    The map of frame numbers to cameras to update
+   *  \param [in]  ext_params A map from frame number to vector of extrinsic
+   *parameters
+   *  \param [in]  int_params A vector of unique camera intrinsic parameter
+   *vectors
+   *  \param [in]  int_map    A map from frame number to index into \p
+   *int_params.
+   *                          The mapping may be many-to-one for shared
+   *intrinsics.
+   *
+   *  The original camera_intrinsic objects are reused if they were not
+   *optimized.
+   *  Otherwise new camera_intrinsic instances are created.
+   *
+   *  This function is the inverse of extract_camera_parameters.
+   */
+  void
+  update_camera_parameters( camera_map::map_camera_t& cameras,
+                            cam_param_map_t const& ext_params,
+                            std::vector< std::vector< double > > const& int_params,
+                            cam_intrinsic_id_map_t const& int_map ) const;
 
   /// type of sharing of intrinsics between cameras to use
   CameraIntrinsicShareType camera_intrinsic_share_type = AUTO_SHARE_INTRINSICS;
