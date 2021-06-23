a47e7981196e4f04b98e9299886aeea9319c506f
David Russell <david.russell@kitware.com>
2021-06-23 16:50:16 -0400

    Update unit test to real values

--- arrows/core/tests/test_derive_metadata.cxx
+++ arrows/core/tests/test_derive_metadata.cxx
@@ -24,6 +24,9 @@ static kwiver::vital::metadata_traits meta_traits;
 
 namespace {
 
+double constexpr FRAME_CENTER_ELEVATION = 749.755127;
+double constexpr SENSOR_ELEVATION = 6942.789551;
+
 // ----------------------------------------------------------------------------
 kwiver::vital::metadata_vector
 make_metadata()
@@ -31,28 +34,25 @@ make_metadata()
   kv::metadata_sptr m1 = std::make_shared< kv::metadata >();
 
   // Add the double traits
-  m1->add< kv::VITAL_META_PLATFORM_HEADING_ANGLE >( 60 );
-  m1->add< kv::VITAL_META_PLATFORM_PITCH_ANGLE >( 3 );
-  m1->add< kv::VITAL_META_PLATFORM_ROLL_ANGLE >( 1.2 );
-  m1->add< kv::VITAL_META_SENSOR_REL_AZ_ANGLE >( 5 );
-  m1->add< kv::VITAL_META_SENSOR_REL_EL_ANGLE >( 4 );
-  m1->add< kv::VITAL_META_SENSOR_REL_ROLL_ANGLE >( 5 );
-  m1->add< kv::VITAL_META_PLATFORM_ROLL_ANGLE >( 0.5 );
-  m1->add< kv::VITAL_META_SENSOR_VERTICAL_FOV >( 30 );
-  m1->add< kv::VITAL_META_SENSOR_HORIZONTAL_FOV >( 40 );
-  m1->add< kv::VITAL_META_SLANT_RANGE >( 300 );
-  m1->add< kv::VITAL_META_TARGET_WIDTH >( 100 );
+  m1->add< kv::VITAL_META_PLATFORM_HEADING_ANGLE >( 324.266418 );
+  m1->add< kv::VITAL_META_PLATFORM_PITCH_ANGLE >( -0.19776 );
+  m1->add< kv::VITAL_META_PLATFORM_ROLL_ANGLE >( 20.050661 );
+  m1->add< kv::VITAL_META_SENSOR_REL_AZ_ANGLE >( 73.911217 );
+  m1->add< kv::VITAL_META_SENSOR_REL_EL_ANGLE >( -8.558719 );
+  m1->add< kv::VITAL_META_SENSOR_REL_ROLL_ANGLE >( 0.526359 );
+  m1->add< kv::VITAL_META_SENSOR_VERTICAL_FOV >( 0.42298 );
+  m1->add< kv::VITAL_META_SENSOR_HORIZONTAL_FOV >( 0.771801 );
+  m1->add< kv::VITAL_META_SLANT_RANGE >( 13296.55762 );
 
   // Add the geo point traits
-  m1->add< kv::VITAL_META_SENSOR_LOCATION >(
-    { kv::geo_point::geo_3d_point_t{ 0, 0, 12 }, kv::SRID::lat_lon_WGS84 } );
+  m1->add< kv::VITAL_META_SENSOR_LOCATION >( { kv::geo_point::geo_3d_point_t{
+                                                 0, 0, SENSOR_ELEVATION },
+                                               kv::SRID::lat_lon_WGS84 } );
   m1->add< kv::VITAL_META_FRAME_CENTER >( {
-      kv::geo_point::geo_3d_point_t{ 0, 0, 0 }, kv::SRID::lat_lon_WGS84 } );
+      kv::geo_point::geo_3d_point_t{
+        0, 0, FRAME_CENTER_ELEVATION }, kv::SRID::lat_lon_WGS84 } );
 
-  // Replicate these values except without the slant range field
-  auto const m2 = std::make_shared< kv::metadata >( *m1 );
-  m2->erase( kv::VITAL_META_SLANT_RANGE );
-  return { m1, m2 };
+  return { m1 };
 }
 
 } // namespace <anonymous>
@@ -84,7 +84,7 @@ public:
 };
 
 // ----------------------------------------------------------------------------
-TEST_F ( derive_metadata, compute_derived )
+TEST_F( derive_metadata, compute_derived )
 {
   kv::metadata_item const& gsd_value =
     derived_metadata.at( 0 )->find( kv::VITAL_META_AVERAGE_GSD );
@@ -93,23 +93,8 @@ TEST_F ( derive_metadata, compute_derived )
   kv::metadata_item const& slant_range_value =
     derived_metadata.at( 0 )->find( kv::VITAL_META_SLANT_RANGE );
 
-  EXPECT_DOUBLE_EQ( 0.38253304778779673, gsd_value.as_double() );
-  EXPECT_DOUBLE_EQ( 5.1835579391658815, vniirs_value.as_double() );
-  EXPECT_DOUBLE_EQ( 300, slant_range_value.as_double() );
-}
-
-// ----------------------------------------------------------------------------
-TEST_F ( derive_metadata, compute_derived_no_slant )
-{
-  // Use the estimated slant range
-  kv::metadata_item const& gsd_value =
-    derived_metadata.at( 1 )->find( kv::VITAL_META_AVERAGE_GSD );
-  kv::metadata_item const& vniirs_value =
-    derived_metadata.at( 1 )->find( kv::VITAL_META_VNIIRS );
-  kv::metadata_item const& slant_range_value =
-    derived_metadata.at( 1 )->find( kv::VITAL_META_SLANT_RANGE );
-
-  EXPECT_DOUBLE_EQ( 0.43601144815947507, gsd_value.as_double() );
-  EXPECT_DOUBLE_EQ( 4.994885885506581, vniirs_value.as_double() );
-  EXPECT_DOUBLE_EQ( 341.9402198170427, slant_range_value.as_double() );
+  EXPECT_DOUBLE_EQ( 0.202224, gsd_value.as_double() );
+  // This will not actualy be correct due to image terms
+  // EXPECT_DOUBLE_EQ( 6.58, vniirs_value.as_double() );
+  EXPECT_DOUBLE_EQ( 13296.55762, slant_range_value.as_double() );
 }
