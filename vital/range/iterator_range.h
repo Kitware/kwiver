d930cc25fdc487872ed1a79cd335127d72049319
Daniel Riehm <daniel.riehm@kitware.com>
2021-10-14 16:14:46 -0400

    Use new vital iterator_range instead of boost

---
+++ vital/range/iterator_range.h
@@ -0,0 +1,43 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+#ifndef VITAL_ITERATOR_RANGE_H
+#define VITAL_ITERATOR_RANGE_H
+
+/// \file Class to wrap two iterators in a range-friendly way.
+
+#include <utility>
+
+namespace kwiver {
+
+namespace vital {
+
+namespace range {
+
+// ----------------------------------------------------------------------------
+/// Wraps two iterators into a range.
+template < class Iterator >
+class iterator_range
+{
+public:
+  iterator_range( Iterator const& begin, Iterator const& end )
+    : m_begin{ begin }, m_end{ end } {}
+  iterator_range( Iterator&& begin, Iterator&& end )
+    : m_begin{ std::move( begin ) }, m_end{ std::move( end ) } {}
+
+  Iterator begin() const { return m_begin; }
+  Iterator end() const { return m_end; }
+
+protected:
+  Iterator m_begin;
+  Iterator m_end;
+};
+
+} // namespace range
+
+} // namespace vital
+
+} // namespace kwiver
+
+#endif
