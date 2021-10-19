9a093e5a37ac2141e96d19fa11584dc82a4f8695
hannah.defazio <hannah.defazio@kitware.com>
2021-10-19 17:20:46 -0400

    Added cross hair detection

---
+++ arrows/burnout/moving_burnin_detector_opencv.h
@@ -0,0 +1,57 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+#ifndef KWIVER_ARROWS_BURNOUT_MOVING_BURNIN_DETECTOR_OPENCV_
+#define KWIVER_ARROWS_BURNOUT_MOVING_BURNIN_DETECTOR_OPENCV_
+
+#include <arrows/burnout/kwiver_algo_burnout_export.h>
+
+#include <vital/algo/image_filter.h>
+
+#include <opencv/cxcore.h>
+
+#include <deque>
+#include <vector>
+
+namespace kwiver {
+
+namespace arrows {
+
+namespace burnout {
+
+class KWIVER_ALGO_BURNOUT_EXPORT moving_burnin_detector_opencv
+  : public vital::algo::image_filter
+{
+  public:
+    PLUGIN_INFO( "burnout_moving_burnin_detector_opencv",
+               "Detect burnin items from an image." )
+
+    moving_burnin_detector_opencv();
+    virtual ~moving_burnin_detector_opencv();
+
+    /// Get this algorithm's \link vital::config_block configuration block
+    /// \endlink
+    virtual vital::config_block_sptr get_configuration() const;
+    /// Set this algorithm's properties via a config block
+    virtual void set_configuration( vital::config_block_sptr config );
+    /// Check that the algorithm's currently configuration is valid
+    virtual bool check_configuration( vital::config_block_sptr config ) const;
+
+    /// detect burnin items
+    virtual kwiver::vital::image_container_sptr filter(
+      kwiver::vital::image_container_sptr image_data );
+
+  private:
+    class priv;
+
+    std::unique_ptr< priv > const d;
+};
+
+} // namespace burnout
+
+} // namespace arrows
+
+} // namespace kwiver
+
+#endif
