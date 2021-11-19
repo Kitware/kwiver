0a4f0b3abd112373b952f04103f80b2867f58228
Daniel Riehm <daniel.riehm@kitware.com>
2021-11-19 13:56:41 -0500

    Refactor MISP timestamp parsing

--- arrows/klv/misp_time.h
+++ arrows/klv/misp_time.h
@@ -2,47 +2,92 @@
 // OSI-approved BSD 3-Clause License. See top-level LICENSE file or
 // https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
-#ifndef KWIVER_ARROWS_KLV_KLV_MISP_TIME_H
-#define KWIVER_ARROWS_KLV_KLV_MISP_TIME_H
+/// \file
+/// \brief Declare MISP timestamp utility functions.
+
+// Code here based on the following standards:
+// https://gwg.nga.mil/misb/docs/standards/ST0603.5.pdf
+// https://gwg.nga.mil/misb/docs/standards/ST0604.3.pdf
+
+#ifndef KWIVER_ARROWS_KLV_MISP_TIME_H
+#define KWIVER_ARROWS_KLV_MISP_TIME_H
 
 #include <arrows/klv/kwiver_algo_klv_export.h>
 
+#include <vital/range/iota.h>
+
+#include <algorithm>
 #include <vector>
+
 #include <cstdint>
 
 namespace kwiver {
+
 namespace arrows {
+
 namespace klv {
 
-/**
- * @brief Find MISP time packet in raw buffer and convert.
- *
- * This function scans the supplied raw video metadata buffer to see
- * if it contains a MISP time packet. If it does, the packet is
- * converted to a time value and returned.
- *
- * @param[in] raw_data Raw metadatadata buffer
- * @param[out] ts Time from MISP packet.
- *
- * @return \b true if MISP time packet found in buffer.
- */
-KWIVER_ALGO_KLV_EXPORT
-bool find_MISP_microsec_time(  std::vector< unsigned char > const& raw_data, std::int64_t& ts );
+namespace misp_detail {
 
-/**
- * @brief Convert MISP time packet to uSec
- *
- * This function converts the supplied MIST time packet to
- * microseconds.
- *
- * @param[in] buf Raw packet to convert
- * @param[out] ts Time from packet in microseconds
- *
- * @return \b true if the buffer passes validity checks.
- */
-KWIVER_ALGO_KLV_EXPORT
-bool convert_MISP_microsec_time( std::vector< unsigned char > const& buf, std::int64_t& ts );
+constexpr ptrdiff_t tag_length = 16;
+constexpr ptrdiff_t status_length = 1;
+constexpr ptrdiff_t timestamp_length = 8 + 3;
+constexpr ptrdiff_t packet_length =
+  tag_length + status_length + timestamp_length;
 
-} } } // end namespace
+} // namespace misp_detail
+
+// ---------------------------------------------------------------------------
+/// Locate a MISP timestamp packet in a sequence of bytes.
+///
+/// \param begin Iterator to beginning of byte sequence.
+/// \param end Iterator to end of byte sequence.
+///
+/// \return Iterator to beginning of MISP packet, or \p end on failure.
+template < class Iterator >
+Iterator
+find_misp_timestamp( Iterator begin, Iterator end )
+{
+  static auto const tag = std::string{ "MISPmicrosectime" };
+  auto const it = std::search( begin, end, tag.cbegin(), tag.cend() );
+  return ( std::distance( it, end ) < misp_detail::packet_length ) ? end : it;
+}
+
+// ---------------------------------------------------------------------------
+/// Read a MISP timestamp from a sequence of bytes.
+///
+/// \param begin Iterator to beginning of MISP packet.
+///
+/// \return MISP microsecond timestamp.
+template < class Iterator >
+uint64_t
+read_misp_timestamp( Iterator begin )
+{
+  // Skip tag and status to get to timestamp
+  auto it = std::next( begin,
+                       misp_detail::tag_length +
+                       misp_detail::status_length );
+  uint64_t result = 0;
+  for( auto const i :
+       kwiver::vital::range::iota( misp_detail::timestamp_length ) )
+  {
+    // Every third byte is set to 0xFF to avoid the timestamp being read as a
+    // start tag for some other sort of data
+    if( i % 3 != 2 )
+    {
+      result <<= 8;
+      result |= *it;
+    }
+    ++it;
+  }
+
+  return result;
+}
+
+} // namespace klv
+
+} // namespace arrows
+
+} // namespace kwiver
 
 #endif
