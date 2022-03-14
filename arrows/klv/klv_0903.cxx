ce52509bc04947d7ab834aee2b6d5f25c23ce2ac
Daniel Riehm <daniel.riehm@kitware.com>
2022-03-14 10:35:10 -0400

    Fix parity bug in ST0903

--- arrows/klv/klv_0903.cxx
+++ arrows/klv/klv_0903.cxx
@@ -177,7 +177,8 @@ klv_0903_local_set_format
 ::calculate_checksum( klv_read_iter_t data, size_t length ) const
 {
   return klv_running_sum_16( checksum_header.begin(), checksum_header.end(),
-                             klv_running_sum_16( data, data + length ) );
+                             klv_running_sum_16( data, data + length ),
+                             length % 2 );
 }
 
 // ----------------------------------------------------------------------------
