de4139d8b132cd5775b6c3acc51c73b70a4d8ee6
OwenMcGee <owenmcgee69@gmail.com>
2022-03-30 15:25:01 -0400

    Updated read tests and added more write tests

--- vital/tests/test_mesh_io.cxx
+++ vital/tests/test_mesh_io.cxx
@@ -7,6 +7,7 @@
 
 #include <kwiversys/SystemTools.hxx>
 #include <tests/test_gtest.h>
+#include <tests/test_scene.h>
 #include <vital/exceptions.h>
 
 #include <vital/io/mesh_io.cxx>
@@ -59,47 +60,154 @@ TEST_F(mesh_io, read_invalid_type)
 // ----------------------------------------------------------------------------
 TEST_F(mesh_io, read_write_ply2)
 {
-  std::string original =
-    data_dir + "/pipeline_data/aphill_240_1fps_crf32_sm_fused_mesh.ply2";
-  std::string copy = "temp/aphill_240_1fps_crf32_sm_fused_mesh2.ply2";
+  kwiver::vital::mesh_sptr original = kwiver::testing::cube_mesh( 1.0 );
 
-  kwiver::vital::mesh_sptr mesh_ply2 = kwiver::vital::read_ply2( original );
-  kwiver::vital::write_ply2( copy, *mesh_ply2 );
+  std::string path = "temp/cube_mesh.ply2";
+  kwiver::vital::write_ply2( path, *original );
+  kwiver::vital::mesh_sptr copy = kwiver::vital::read_ply2( path );
 
-  std::ifstream original_stream(original.c_str());
-  std::ifstream copy_stream(copy.c_str());
+  EXPECT_EQ( original->num_edges(), copy->num_edges() );
 
-  EXPECT_EQ( original_stream.gcount(), copy_stream.gcount() );
+  kwiver::vital::mesh_vertex_array<3>& original_vertices = dynamic_cast
+    <kwiver::vital::mesh_vertex_array<3>&>( original->vertices() );
+  kwiver::vital::mesh_vertex_array<3>& copy_vertices = dynamic_cast
+    <kwiver::vital::mesh_vertex_array<3>&>( copy->vertices() );
 
-  std::string original_str = "";
-  std::string copy_str = "";
-  while( original_stream >> original_str && copy_stream >> copy_str ){
-    EXPECT_EQ( original_str, copy_str );
+  EXPECT_EQ( original_vertices.size(), copy_vertices.size() );
+  for(unsigned int i=0; i<original_vertices.size(); i++)
+  {
+    EXPECT_EQ( original_vertices[i].size(), copy_vertices[i].size() );
+    for(unsigned int j=0; j<original_vertices[i].size(); j++)
+    {
+      EXPECT_EQ( original_vertices[i][j], copy_vertices[i][j] );
+    }
+  }
+
+  kwiver::vital::mesh_face_array original_faces = *std::make_shared
+    <kwiver::vital::mesh_face_array>(original->faces());
+  kwiver::vital::mesh_face_array copy_faces = *std::make_shared
+    <kwiver::vital::mesh_face_array>(copy->faces());
+
+  EXPECT_EQ( original_faces.size(), copy_faces.size() );
+  for(unsigned int i=0; i<original_faces.size(); i++)
+  {
+    EXPECT_EQ( original_faces[i].size(), copy_faces[i].size() );
+    for(unsigned int j=0; j<original_faces[i].size(); j++)
+    {
+      EXPECT_EQ( original_faces[i][j], copy_faces[i][j] );
+    }
   }
 
   kwiversys::SystemTools::RemoveADirectory( "temp" );
 }
 
 // ----------------------------------------------------------------------------
-TEST_F(mesh_io, read_write_obj)
+TEST_F(mesh_io, read_ply)
 {
-  std::string original =
-    data_dir + "/pipeline_data/aphill_240_1fps_crf32_sm_fused_mesh.obj";
-  std::string copy = "temp/aphill_240_1fps_crf32_sm_fused_mesh2.obj";
+  kwiver::vital::mesh_sptr ply_mesh = kwiver::vital::read_ply(
+    data_dir + "/cube_mesh.ply");
+  kwiver::vital::mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
 
-  kwiver::vital::mesh_sptr mesh_obj = kwiver::vital::read_obj( original );
-  kwiver::vital::write_obj( copy, *mesh_obj );
+  EXPECT_EQ( ply_mesh->num_edges(), cube_mesh->num_edges() );
 
-  std::ifstream original_stream(original.c_str());
-  std::ifstream copy_stream(copy.c_str());
+  kwiver::vital::mesh_vertex_array<3>& ply_mesh_vertices = dynamic_cast
+    <kwiver::vital::mesh_vertex_array<3>&>( ply_mesh->vertices() );
+  kwiver::vital::mesh_vertex_array<3>& cube_mesh_vertices = dynamic_cast
+    <kwiver::vital::mesh_vertex_array<3>&>( cube_mesh->vertices() );
 
-  EXPECT_EQ( original_stream.gcount(), copy_stream.gcount() );
-
-  std::string original_str = "";
-  std::string copy_str = "";
-  while( original_stream >> original_str && copy_stream >> copy_str ){
-    EXPECT_EQ( original_str, copy_str );
+  EXPECT_EQ( ply_mesh_vertices.size(), cube_mesh_vertices.size() );
+  for(unsigned int i=0; i<ply_mesh_vertices.size(); i++)
+  {
+    EXPECT_EQ( ply_mesh_vertices[i].size(), cube_mesh_vertices[i].size() );
+    for(unsigned int j=0; j<ply_mesh_vertices[i].size(); j++)
+    {
+      EXPECT_EQ( ply_mesh_vertices[i][j], cube_mesh_vertices[i][j] );
+    }
   }
 
+  kwiver::vital::mesh_face_array ply_mesh_faces = *std::make_shared
+    <kwiver::vital::mesh_face_array>(ply_mesh->faces());
+  kwiver::vital::mesh_face_array cube_mesh_faces = *std::make_shared
+    <kwiver::vital::mesh_face_array>(cube_mesh->faces());
+
+  EXPECT_EQ( ply_mesh_faces.size(), cube_mesh_faces.size() );
+  for(unsigned int i=0; i<ply_mesh_faces.size(); i++)
+  {
+    EXPECT_EQ( ply_mesh_faces[i].size(), cube_mesh_faces[i].size() );
+    for(unsigned int j=0; j<ply_mesh_faces[i].size(); j++)
+    {
+      EXPECT_EQ( ply_mesh_faces[i][j], cube_mesh_faces[i][j] );
+    }
+  }
+}
+
+// ----------------------------------------------------------------------------
+TEST_F(mesh_io, read_write_obj)
+{
+  kwiver::vital::mesh_sptr original = kwiver::testing::cube_mesh( 1.0 );
+
+  std::string path = "temp/cube_mesh.obj";
+  kwiver::vital::write_obj( path, *original );
+  kwiver::vital::mesh_sptr copy = kwiver::vital::read_obj( path );
+
+  EXPECT_EQ( original->num_edges(), copy->num_edges() );
+
+  kwiver::vital::mesh_vertex_array<3>& original_vertices = dynamic_cast
+    <kwiver::vital::mesh_vertex_array<3>&>( original->vertices() );
+  kwiver::vital::mesh_vertex_array<3>& copy_vertices = dynamic_cast
+    <kwiver::vital::mesh_vertex_array<3>&>( copy->vertices() );
+
+  EXPECT_EQ( original_vertices.size(), copy_vertices.size() );
+  for(unsigned int i=0; i<original_vertices.size(); i++)
+  {
+    EXPECT_EQ( original_vertices[i].size(), copy_vertices[i].size() );
+    for(unsigned int j=0; j<original_vertices[i].size(); j++)
+    {
+      EXPECT_EQ( original_vertices[i][j], copy_vertices[i][j] );
+    }
+  }
+
+  kwiver::vital::mesh_face_array original_faces = *std::make_shared
+    <kwiver::vital::mesh_face_array>(original->faces());
+  kwiver::vital::mesh_face_array copy_faces = *std::make_shared
+    <kwiver::vital::mesh_face_array>(copy->faces());
+
+  EXPECT_EQ( original_faces.size(), copy_faces.size() );
+  for(unsigned int i=0; i<original_faces.size(); i++)
+  {
+    EXPECT_EQ( original_faces[i].size(), copy_faces[i].size() );
+    for(unsigned int j=0; j<original_faces[i].size(); j++)
+    {
+      EXPECT_EQ( original_faces[i][j], copy_faces[i][j] );
+    }
+  }
+
+  kwiversys::SystemTools::RemoveADirectory( "temp" );
+}
+
+// ----------------------------------------------------------------------------
+TEST_F(mesh_io, write_kml)
+{
+  kwiver::vital::mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  EXPECT_NO_THROW( kwiver::vital::write_kml(
+    "temp/cube_mesh.kml", *cube_mesh ) );
+  kwiversys::SystemTools::RemoveADirectory( "temp" );
+}
+
+// ----------------------------------------------------------------------------
+TEST_F(mesh_io, write_kml_collada)
+{
+  kwiver::vital::mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  EXPECT_NO_THROW( kwiver::vital::write_kml_collada(
+    "temp/cube_mesh.kml_collada", *cube_mesh ) );
+  kwiversys::SystemTools::RemoveADirectory( "temp" );
+}
+
+// ----------------------------------------------------------------------------
+TEST_F(mesh_io, write_vrml)
+{
+  kwiver::vital::mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  EXPECT_NO_THROW( kwiver::vital::write_vrml(
+    "temp/cube_mesh.vrml", *cube_mesh ) );
   kwiversys::SystemTools::RemoveADirectory( "temp" );
 }
