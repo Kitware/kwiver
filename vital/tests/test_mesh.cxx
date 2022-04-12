f2cc450389b33c96552d96ead0800932ffa899e4
OwenMcGee <owenmcgee69@gmail.com>
2022-04-12 13:37:21 -0400

    Made normals tests more rigorous

--- vital/tests/test_mesh.cxx
+++ vital/tests/test_mesh.cxx
@@ -13,7 +13,9 @@
 
 #include <gtest/gtest.h>
 
-kwiver::vital::path_t g_data_dir;
+using namespace kwiver::vital;
+
+path_t g_data_dir;
 
 // ----------------------------------------------------------------------------
 int main(int argc, char** argv)
@@ -25,9 +27,9 @@ int main(int argc, char** argv)
 // ----------------------------------------------------------------------------
 TEST(mesh, group_names)
 {
-  kwiver::vital::mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
-  std::shared_ptr<kwiver::vital::mesh_face_array> faces =
-    std::make_shared<kwiver::vital::mesh_face_array>(cube_mesh->faces());
+  mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  std::shared_ptr<mesh_face_array> faces =
+    std::make_shared<mesh_face_array>(cube_mesh->faces());
 
   for(unsigned int i=0; i<faces->size(); i++){
     EXPECT_EQ( faces->group_name(i), "" );
@@ -66,23 +68,23 @@ TEST(mesh, group_names)
 // ----------------------------------------------------------------------------
 TEST(mesh, append)
 {
-  kwiver::vital::mesh_sptr first_mesh = kwiver::testing::cube_mesh( 1.0 );
+  mesh_sptr first_mesh = kwiver::testing::cube_mesh( 1.0 );
   first_mesh->compute_face_normals();
-  std::shared_ptr<kwiver::vital::mesh_face_array> first_faces =
-    std::make_shared<kwiver::vital::mesh_face_array>(first_mesh->faces());
+  std::shared_ptr<mesh_face_array> first_faces =
+    std::make_shared<mesh_face_array>(first_mesh->faces());
   unsigned int first_size = first_faces->size();
   EXPECT_EQ( first_faces->make_group( "first name" ), first_size );
 
-  kwiver::vital::mesh_sptr second_mesh = kwiver::testing::grid_mesh( 2, 3 );
+  mesh_sptr second_mesh = kwiver::testing::grid_mesh( 2, 3 );
   second_mesh->compute_face_normals();
-  std::shared_ptr<kwiver::vital::mesh_face_array> second_faces =
-    std::make_shared<kwiver::vital::mesh_face_array>(second_mesh->faces());
+  std::shared_ptr<mesh_face_array> second_faces =
+    std::make_shared<mesh_face_array>(second_mesh->faces());
   unsigned int second_size = second_faces->size();
   EXPECT_EQ( second_faces->make_group( "second name" ), second_size );
 
-  kwiver::vital::mesh_sptr third_mesh = kwiver::testing::cube_mesh( 1.0 );
-  std::shared_ptr<kwiver::vital::mesh_face_array> third_faces =
-    std::make_shared<kwiver::vital::mesh_face_array>(third_mesh->faces());
+  mesh_sptr third_mesh = kwiver::testing::cube_mesh( 1.0 );
+  std::shared_ptr<mesh_face_array> third_faces =
+    std::make_shared<mesh_face_array>(third_mesh->faces());
   unsigned int third_size = third_faces->size();
 
   first_faces->append( *second_faces );
@@ -105,8 +107,8 @@ TEST(mesh, append_with_shift)
                                                          { 5, 6, 7, 8, 9 } };
   unsigned int shift = 10;
 
-  kwiver::vital::mesh_face_array first_faces( first_list );
-  kwiver::vital::mesh_face_array second_faces( second_list );
+  mesh_face_array first_faces( first_list );
+  mesh_face_array second_faces( second_list );
 
   first_faces.append( second_faces, shift );
 
@@ -131,7 +133,7 @@ TEST(mesh, append_with_shift)
 // ----------------------------------------------------------------------------
 TEST(mesh, half_edges)
 {
-  kwiver::vital::mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
 
   std::vector<std::vector<unsigned int>> face_list = { {0, 1, 3, 2},
                                                        {4, 6, 7, 5},
@@ -145,11 +147,11 @@ TEST(mesh, half_edges)
     list_size+=face_list[i].size();
   }
 
-  kwiver::vital::mesh_half_edge_set constructed_edges( face_list );
+  mesh_half_edge_set constructed_edges( face_list );
   EXPECT_FALSE( cube_mesh->has_half_edges() );
   cube_mesh->build_edge_graph();
   EXPECT_TRUE( cube_mesh->has_half_edges() );
-  kwiver::vital::mesh_half_edge_set copy_edges = cube_mesh->half_edges();
+  mesh_half_edge_set copy_edges = cube_mesh->half_edges();
 
   EXPECT_EQ( constructed_edges.num_verts(), copy_edges.num_verts() );
   EXPECT_EQ( constructed_edges.num_faces(), copy_edges.num_faces() );
@@ -161,15 +163,14 @@ TEST(mesh, half_edges)
 // ----------------------------------------------------------------------------
 TEST(mesh, copy_constructor)
 {
-  kwiver::vital::mesh_sptr original = kwiver::testing::cube_mesh( 1.0 );
-  kwiver::vital::mesh_sptr copy =
-    std::make_shared<kwiver::vital::mesh>( *original );
+  mesh_sptr original = kwiver::testing::cube_mesh( 1.0 );
+  mesh_sptr copy = std::make_shared<mesh>( *original );
 
   EXPECT_EQ( *original, *copy );
 
   EXPECT_TRUE( original->is_init() );
   EXPECT_TRUE( copy->is_init() );
-  *original = kwiver::vital::mesh();
+  *original = mesh();
   EXPECT_FALSE( original->is_init() );
   EXPECT_TRUE( copy->is_init() );
 }
@@ -177,16 +178,15 @@ TEST(mesh, copy_constructor)
 // ----------------------------------------------------------------------------
 TEST(mesh, assignment_operator)
 {
-  kwiver::vital::mesh_sptr original = kwiver::testing::cube_mesh( 1.0 );
-  kwiver::vital::mesh_sptr copy =
-    std::make_shared<kwiver::vital::mesh>();
-  *copy = kwiver::vital::mesh( *original );
+  mesh_sptr original = kwiver::testing::cube_mesh( 1.0 );
+  mesh_sptr copy = std::make_shared<mesh>();
+  *copy = *original;
 
   EXPECT_EQ( *original, *copy );
 
   EXPECT_TRUE( original->is_init() );
   EXPECT_TRUE( copy->is_init() );
-  *original = kwiver::vital::mesh();
+  *original = mesh();
   EXPECT_FALSE( original->is_init() );
   EXPECT_TRUE( copy->is_init() );
 }
@@ -194,20 +194,58 @@ TEST(mesh, assignment_operator)
 // ----------------------------------------------------------------------------
 TEST(mesh, compute_vertex_normals)
 {
-  kwiver::vital::mesh_sptr grid_mesh = kwiver::testing::grid_mesh( 2, 3 );
+  double threshhold = 0.000001;
 
+  mesh_sptr grid_mesh = kwiver::testing::grid_mesh( 2, 3 );
   EXPECT_FALSE( grid_mesh->vertices().has_normals() );
   EXPECT_FALSE( grid_mesh->has_half_edges() );
   grid_mesh->compute_vertex_normals();
   EXPECT_TRUE( grid_mesh->vertices().has_normals() );
   EXPECT_TRUE( grid_mesh->has_half_edges() );
+
+  const std::vector<vector_3d> grid_vertex_normals = grid_mesh->vertices().normals();
+  vector_3d expected_grid_vertex_normal {0, 0, 1};
+  for(unsigned int i=0; i<grid_vertex_normals.size(); i++)
+  {
+    EXPECT_EQ( grid_vertex_normals[i], expected_grid_vertex_normal );
+  }
+
+  mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  EXPECT_FALSE( cube_mesh->vertices().has_normals() );
+  EXPECT_FALSE( cube_mesh->has_half_edges() );
+  cube_mesh->compute_vertex_normals();
+  EXPECT_TRUE( cube_mesh->vertices().has_normals() );
+  EXPECT_TRUE( cube_mesh->has_half_edges() );
+
+  const std::vector<vector_3d> cube_vertex_normals =
+    cube_mesh->vertices().normals();
+  std::vector<vector_3d> expected_cube_vertex_normals{
+    {-0.57735, -0.57735, -0.57735},
+    {-0.57735, -0.57735,  0.57735},
+    {-0.57735,  0.57735, -0.57735},
+    {-0.57735,  0.57735,  0.57735},
+    { 0.57735, -0.57735, -0.57735},
+    { 0.57735, -0.57735,  0.57735},
+    { 0.57735,  0.57735, -0.57735},
+    { 0.57735,  0.57735,  0.57735} };
+  EXPECT_EQ( cube_vertex_normals.size(), expected_cube_vertex_normals.size() );
+  for(unsigned int i=0; i<cube_vertex_normals.size(); i++)
+  {
+    EXPECT_NEAR( cube_vertex_normals[i][0],
+      expected_cube_vertex_normals[i][0], threshhold );
+    EXPECT_NEAR( cube_vertex_normals[i][1],
+      expected_cube_vertex_normals[i][1], threshhold );
+    EXPECT_NEAR( cube_vertex_normals[i][2],
+      expected_cube_vertex_normals[i][2], threshhold );
+  }
 }
 
 // ----------------------------------------------------------------------------
 TEST(mesh, compute_vertex_normals_from_faces)
 {
-  kwiver::vital::mesh_sptr grid_mesh = kwiver::testing::grid_mesh( 2, 3 );
+  double threshhold = 0.000001;
 
+  mesh_sptr grid_mesh = kwiver::testing::grid_mesh( 2, 3 );
   EXPECT_FALSE( grid_mesh->vertices().has_normals() );
   EXPECT_FALSE( grid_mesh->faces().has_normals() );
   EXPECT_FALSE( grid_mesh->has_half_edges() );
@@ -215,4 +253,64 @@ TEST(mesh, compute_vertex_normals_from_faces)
   EXPECT_TRUE( grid_mesh->vertices().has_normals() );
   EXPECT_TRUE( grid_mesh->faces().has_normals() );
   EXPECT_TRUE( grid_mesh->has_half_edges() );
+
+  const std::vector<vector_3d> grid_vertex_normals = grid_mesh->vertices().normals();
+  vector_3d expected_grid_vertex_normal {0, 0, 1};
+  for(unsigned int i=0; i<grid_vertex_normals.size(); i++)
+  {
+    EXPECT_EQ( grid_vertex_normals[i], expected_grid_vertex_normal );
+  }
+
+  const std::vector<vector_3d> grid_face_normals = grid_mesh->faces().normals();
+  vector_3d expected_grid_face_normal {0, 0, 1};
+  for(unsigned int i=0; i<grid_face_normals.size(); i++)
+  {
+    EXPECT_EQ( grid_face_normals[i], expected_grid_face_normal );
+  }
+
+  mesh_sptr cube_mesh = kwiver::testing::cube_mesh( 1.0 );
+  EXPECT_FALSE( cube_mesh->vertices().has_normals() );
+  EXPECT_FALSE( cube_mesh->faces().has_normals() );
+  EXPECT_FALSE( cube_mesh->has_half_edges() );
+  cube_mesh->compute_vertex_normals_from_faces();
+  EXPECT_TRUE( cube_mesh->vertices().has_normals() );
+  EXPECT_TRUE( cube_mesh->faces().has_normals() );
+  EXPECT_TRUE( cube_mesh->has_half_edges() );
+
+  const std::vector<vector_3d> cube_vertex_normals =
+    cube_mesh->vertices().normals();
+  std::vector<vector_3d> expected_cube_vertex_normals{
+    {-0.57735, -0.57735, -0.57735},
+    {-0.57735, -0.57735,  0.57735},
+    {-0.57735,  0.57735, -0.57735},
+    {-0.57735,  0.57735,  0.57735},
+    { 0.57735, -0.57735, -0.57735},
+    { 0.57735, -0.57735,  0.57735},
+    { 0.57735,  0.57735, -0.57735},
+    { 0.57735,  0.57735,  0.57735} };
+  EXPECT_EQ( cube_vertex_normals.size(), expected_cube_vertex_normals.size() );
+  for(unsigned int i=0; i<cube_vertex_normals.size(); i++)
+  {
+    EXPECT_NEAR( cube_vertex_normals[i][0],
+      expected_cube_vertex_normals[i][0], threshhold );
+    EXPECT_NEAR( cube_vertex_normals[i][1],
+      expected_cube_vertex_normals[i][1], threshhold );
+    EXPECT_NEAR( cube_vertex_normals[i][2],
+      expected_cube_vertex_normals[i][2], threshhold );
+  }
+
+  const std::vector<vector_3d> cube_face_normals =
+    cube_mesh->faces().normals();
+  std::vector<vector_3d> expected_cube_face_normals{
+    {-1,  0,  0},
+    { 1,  0,  0},
+    { 0,  0,  1},
+    { 0,  0, -1},
+    { 0,  1,  0},
+    { 0, -1,  0} };
+  EXPECT_EQ( cube_face_normals.size(), expected_cube_face_normals.size() );
+  for(unsigned int i=0; i<cube_face_normals.size(); i++)
+  {
+    EXPECT_EQ( cube_face_normals[i], expected_cube_face_normals[i] );
+  }
 }
