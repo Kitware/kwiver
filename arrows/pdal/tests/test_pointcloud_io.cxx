dfeec754c4ba6ea344494ffcecdc85f7bcbf0323
Chet Nieter <chet.nieter@gmail.com>
2022-05-12 11:16:34 -0400

    Added test for new load method of pdal pointcloud_io

--- arrows/pdal/tests/test_pointcloud_io.cxx
+++ arrows/pdal/tests/test_pointcloud_io.cxx
@@ -10,6 +10,7 @@
 
 #include <arrows/pdal/algo/pointcloud_io.h>
 #include <arrows/proj/geo_conv.h>
+#include <vital/exceptions/io.h>
 #include <vital/io/landmark_map_io.h>
 #include <vital/plugin_loader/plugin_manager.h>
 
@@ -21,15 +22,17 @@ kv::path_t g_data_dir;
 
 static std::string geo_origin_file = "pointcloud_data/geo_origin.txt";
 static std::string landmarks_file = "pointcloud_data/landmarks.ply";
+static std::string octahedron_base = "pointcloud_data/octahedron";
 static std::string tmp_file = "pointcloud_data/pointcloud.las";
 
 // ----------------------------------------------------------------------------
-int
-main(int argc, char** argv)
-{
-  ::testing::InitGoogleTest(&argc, argv);
 
-  GET_ARG(1, g_data_dir);
+int
+main( int argc, char** argv )
+{
+  ::testing::InitGoogleTest( &argc, argv );
+
+  GET_ARG( 1, g_data_dir );
 
   return RUN_ALL_TESTS();
 }
@@ -37,22 +40,67 @@ main(int argc, char** argv)
 // ----------------------------------------------------------------------------
 class pointcloud_io : public ::testing::Test
 {
-  TEST_ARG(data_dir);
+  TEST_ARG( data_dir );
 };
 
 // ----------------------------------------------------------------------------
-TEST_F(pointcloud_io, create)
+TEST_F ( pointcloud_io, create )
 {
   kv::plugin_manager::instance().load_all_plugins();
 
-  EXPECT_NE(nullptr, kv::algo::pointcloud_io::create("pdal"));
+  EXPECT_NE( nullptr, kv::algo::pointcloud_io::create( "pdal" ) );
 }
 
-TEST_F(pointcloud_io, save) {
+TEST_F ( pointcloud_io, load )
+{
+  std::vector< kwiver::vital::vector_3d > points = {
+    kwiver::vital::vector_3d( -1.0, 1.0, 0.0 ),
+    kwiver::vital::vector_3d( -1.0, -1.0, 0.0 ),
+    kwiver::vital::vector_3d( 1.0, -1.0, 0.0 ),
+    kwiver::vital::vector_3d( 1.0, 1.0, 0.0 ),
+    kwiver::vital::vector_3d( 0.0, 0.0, 0.7 ),
+    kwiver::vital::vector_3d( 0.0, 0.0, -0.7 ) };
+
+  std::vector< kwiver::vital::rgb_color > test_colors = {
+    kwiver::vital::rgb_color( 255, 0, 0 ),
+    kwiver::vital::rgb_color( 0, 255, 0 ),
+    kwiver::vital::rgb_color( 0, 0, 255 ),
+    kwiver::vital::rgb_color( 255, 0, 255 ),
+    kwiver::vital::rgb_color( 255, 255, 255 ),
+    kwiver::vital::rgb_color( 0, 0, 0 ) };
+
+  auto pc_io = kwiver::arrows::pdal::pointcloud_io();
+
+  for( std::string ext : { ".bpf", ".las", ".ply" } )
+  {
+    auto const octahedron_path = data_dir + "/" + octahedron_base + ext;
+
+    auto pc_data = pc_io.load( octahedron_path );
+
+    auto positions = pc_data.positions();
+    auto colors = pc_data.colors();
+
+    EXPECT_EQ( positions.size(), points.size() );
+
+    for( unsigned int i = 0; i < positions.size(); ++i )
+    {
+      EXPECT_TRUE( positions[ i ].isApprox( points[ i ], 0.0000001 ) );
+      EXPECT_EQ( colors[ i ], test_colors[ i ] );
+    }
+  }
+
+  auto const octahedron_path = data_dir + "/" + octahedron_base + ".not";
+
+  EXPECT_THROW( pc_io.load( octahedron_path ),
+                kwiver::vital::invalid_file );
+}
+
+TEST_F ( pointcloud_io, save )
+{
   auto const geo_origin_path = data_dir + "/" + geo_origin_file;
   auto const landmarks_path = data_dir + "/" + landmarks_file;
   auto const tmp_path = data_dir + "/" + tmp_file;
-  std::ofstream ofs(tmp_path);
+  std::ofstream ofs( tmp_path );
   ofs.close();
 
   // This will delete the temporary file even if an exception is thrown
@@ -62,39 +110,40 @@ TEST_F(pointcloud_io, save) {
     {
       std::remove( tmp_path.c_str() );
     }
+
     std::string tmp_path;
-  }
-  tmp_file_deleter{ tmp_path };
+  } tmp_file_deleter{ tmp_path };
 
   kv::landmark_map_sptr landmark_map =
-    kv::read_ply_file(landmarks_path);
+    kv::read_ply_file( landmarks_path );
 
-  std::vector<kv::vector_3d> points;
-  std::vector<kv::rgb_color> colors;
-  points.reserve(landmark_map->size());
-  colors.reserve(landmark_map->size());
-  for (auto lm : landmark_map->landmarks())
+  std::vector< kv::vector_3d > points;
+  std::vector< kv::rgb_color > colors;
+  points.reserve( landmark_map->size() );
+  colors.reserve( landmark_map->size() );
+  for( auto lm : landmark_map->landmarks() )
   {
-    points.push_back(lm.second->loc());
-    colors.push_back(lm.second->color());
+    points.push_back( lm.second->loc() );
+    colors.push_back( lm.second->color() );
   }
 
   static auto geo_conv = kwiver::arrows::proj::geo_conversion{};
-  kv::set_geo_conv(&geo_conv);
+  kv::set_geo_conv( &geo_conv );
 
   auto lgcs = kv::local_geo_cs();
-  read_local_geo_cs_from_file(lgcs, geo_origin_path);
+  read_local_geo_cs_from_file( lgcs, geo_origin_path );
 
   auto pc_io = kwiver::arrows::pdal::pointcloud_io();
-  pc_io.set_local_geo_cs(lgcs);
-  pc_io.save(tmp_path, points, colors);
+  pc_io.set_local_geo_cs( lgcs );
+  pc_io.save( tmp_path, points, colors );
 }
 
-TEST_F(pointcloud_io, save_landmarks) {
+TEST_F ( pointcloud_io, save_landmarks )
+{
   auto const geo_origin_path = data_dir + "/" + geo_origin_file;
   auto const landmarks_path = data_dir + "/" + landmarks_file;
   auto const tmp_path = data_dir + "/" + tmp_file;
-  std::ofstream ofs(tmp_path);
+  std::ofstream ofs( tmp_path );
   ofs.close();
 
   // This will delete the temporary file even if an exception is thrown
@@ -104,20 +153,20 @@ TEST_F(pointcloud_io, save_landmarks) {
     {
       std::remove( tmp_path.c_str() );
     }
+
     std::string tmp_path;
-  }
-  tmp_file_deleter{ tmp_path };
+  } tmp_file_deleter{ tmp_path };
 
   kv::landmark_map_sptr landmark_map =
-    kv::read_ply_file(landmarks_path);
+    kv::read_ply_file( landmarks_path );
 
   static auto geo_conv = kwiver::arrows::proj::geo_conversion{};
-  kv::set_geo_conv(&geo_conv);
+  kv::set_geo_conv( &geo_conv );
 
   auto lgcs = kv::local_geo_cs();
-  read_local_geo_cs_from_file(lgcs, geo_origin_path);
+  read_local_geo_cs_from_file( lgcs, geo_origin_path );
 
   auto pc_io = kwiver::arrows::pdal::pointcloud_io();
-  pc_io.set_local_geo_cs(lgcs);
-  pc_io.save(tmp_path, landmark_map);
+  pc_io.set_local_geo_cs( lgcs );
+  pc_io.save( tmp_path, landmark_map );
 }
