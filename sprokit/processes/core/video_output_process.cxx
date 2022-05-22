0778d42cbc0d4f41a9de4fa7b21d05334e3471b7
Matt Dawkins <matt.dawkins@kitware.com>
2022-05-22 11:32:58 -0400

    First pass chunk option

--- sprokit/processes/core/video_output_process.cxx
+++ sprokit/processes/core/video_output_process.cxx
@@ -21,6 +21,9 @@
 #include <arrows/ffmpeg/ffmpeg_video_settings.h>
 #endif
 
+#include <algorithm>
+#include <string>
+
 namespace algo = kwiver::vital::algo;
 
 namespace kwiver {
@@ -35,6 +38,11 @@ create_config_trait( exit_on_invalid, bool, "false",
   "first frame cannot be read, always exit regardless of this "
   "setting." );
 
+create_config_trait( maximum_length, double, "-1.0",
+  "Maximum output video length (in seconds) if this length is "
+  "exceeded, multiple video files less than this amount will be "
+  "output with a timestamp start extension." );
+
 create_algorithm_name_config_trait( video_writer );
 
 // -----------------------------------------------------------------------------
@@ -46,15 +54,18 @@ public:
   ~priv();
 
   // Configuration values
-  std::string                           m_video_filename;
-  bool                                  m_exit_on_invalid;
+  std::string                            m_video_filename;
+  bool                                   m_exit_on_invalid;
 
   kwiver::vital::algo::video_output_sptr m_video_writer;
-  kwiver::vital::algorithm_capabilities m_video_traits;
-  kwiver::vital::image_container_sptr   m_last_frame;
-  bool                                  m_first_frame;
+  kwiver::vital::algorithm_capabilities  m_video_traits;
+  double                                 m_maximum_length;
 
-  kwiver::vital::metadata_vector        m_last_metadata;
+  double                                 m_frame_rate;
+  bool                                   m_is_first_frame;
+  double                                 m_clip_start_time;
+  kwiver::vital::image_container_sptr    m_last_frame;
+  kwiver::vital::metadata_vector         m_last_metadata;
 
 }; // end priv class
 
@@ -88,6 +99,7 @@ void video_output_process
   // Examine the configuration
   d->m_video_filename = config_value_using_trait( video_filename );
   d->m_exit_on_invalid = config_value_using_trait( exit_on_invalid );
+  d->m_maximum_length = config_value_using_trait( maximum_length );
 
   vital::config_block_sptr algo_config = get_config();
 
@@ -119,7 +131,8 @@ void video_output_process
 {
   scoped_init_instrumentation();
 
-  d->m_first_frame = true;
+  d->m_is_first_frame = true;
+  d->m_clip_start_time = -1.0;
 }
 
 
@@ -127,6 +140,8 @@ void video_output_process
 void video_output_process
 ::_step()
 {
+  bool reset = false;
+
   vital::image_container_sptr frame = grab_from_port_using_trait( image );
   vital::timestamp ts = grab_from_port_using_trait( timestamp );
 
@@ -134,8 +149,7 @@ void video_output_process
   {
     if( d->m_exit_on_invalid )
     {
-      VITAL_THROW( sprokit::invalid_configuration_exception, name(),
-                   "Invalid image received" );
+      VITAL_THROW( vital::image_exception, "Invalid image received" );
     }
     else
     {
@@ -147,27 +161,58 @@ void video_output_process
     d->m_last_frame = frame;
   }
 
-  if( d->m_first_frame )
+  if( d->m_is_first_frame && has_input_port_edge_using_trait( frame_rate ) )
   {
-    double frame_rate;
+    d->m_frame_rate = grab_from_port_using_trait( frame_rate );
+  }
 
-    if( has_input_port_edge_using_trait( frame_rate ) )
+  if( d->m_maximum_length > 0.0 )
+  {
+    double current_seconds = ts.get_time_seconds();
+    double clip_start_time = d->m_maximum_length *
+      static_cast< int >( current_seconds / d->m_maximum_length );
+
+    if( clip_start_time != d->m_clip_start_time )
     {
-      frame_rate = grab_from_port_using_trait( frame_rate );
+      d->m_clip_start_time = clip_start_time;
+      reset = true;
     }
+  }
 
+  if( d->m_is_first_frame || reset )
+  {
     // instantiate a video reader
 #ifdef WITH_FFMPEG
     arrows::ffmpeg::ffmpeg_video_settings default_settings;
-    default_settings.frame_rate = av_d2q( frame_rate, 1e9 );
+    default_settings.frame_rate = av_d2q( d->m_frame_rate, 1e9 );
     default_settings.parameters->width = frame->width();
     default_settings.parameters->height = frame->height();
 #else
     vital::video_settings default_settings;
 #endif
-    d->m_video_writer->open( d->m_video_filename, &default_settings ); // throws
+    std::string filename = d->m_video_filename;
 
-    d->m_first_frame = false;
+    if( reset )
+    {
+      std::size_t pos = filename.find_last_of( "." );
+      std::string stem = filename.substr( 0, pos );
+      std::string ext = ( pos == std::string::npos ? "mp4" : filename.substr( pos ) );
+
+      unsigned seconds_int = static_cast< unsigned >( d->m_clip_start_time );
+      std::string h = std::to_string( seconds_int / 3600 );
+      std::string m = std::to_string( ( seconds_int / 60 ) % 60 );
+      std::string s = std::to_string( seconds_int % 60 );
+
+      std::string time_str = "_" +
+        std::string( 2 - std::min( std::size_t( 2 ), h.length() ), '0' ) + h + "h_" +
+        std::string( 2 - std::min( std::size_t( 2 ), m.length() ), '0' ) + m + "m_" +
+        std::string( 2 - std::min( std::size_t( 2 ), s.length() ), '0' ) + s + "s";
+
+      filename = stem + time_str + ext;
+    }
+
+    d->m_video_writer->open( filename, &default_settings ); // throws
+    d->m_is_first_frame = false;
   }
 
   d->m_video_writer->add_image( frame, ts );
@@ -218,7 +263,8 @@ void video_output_process
 video_output_process::priv
 ::priv()
   : m_exit_on_invalid( true ),
-    m_first_frame( true )
+    m_is_first_frame( true ),
+    m_clip_start_time( -1.0 )
 {
 }
 
