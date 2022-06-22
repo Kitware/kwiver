2531460f4817dedd6784dc2384e72dc9ca03c680
Paul Tunison <paul.tunison@kitware.com>
2022-06-22 19:29:13 -0400

    Remove from_config from applet interface
    Implementations should be responsible for implementing from_config, not
    this abstract interface.

--- vital/applets/kwiver_applet.h
+++ vital/applets/kwiver_applet.h
@@ -110,16 +110,6 @@ public:
    */
   std::unique_ptr< cxxopts::Options > m_cmd_options;
 
-  // It is expected that implementations will not be constructed meaningfully
-  // from config blocks but instead use the input provided by command-line
-  // args.
-  template < typename T >
-  static vital::pluggable_sptr
-  from_config( vital::config_block const& cb )
-  {
-    return std::make_shared< T >()();
-  }
-
 protected:
   /**
    * @brief Get applet name
