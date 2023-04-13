6d5170d32b40c394d32195a050a6c83ecd198908
Daniel Riehm <daniel.riehm@kitware.com>
2023-04-13 11:51:47 -0400

    Add metadata_istream_from_video

--- vital/tests/test_metadata_stream_from_map.cxx
+++ vital/tests/test_metadata_stream_from_map.cxx
@@ -5,59 +5,8 @@
 /// \file
 /// Tests for the metadata_[io]stream_from_map classes.
 
+#include <vital/tests/test_metadata_stream.h>
 #include <vital/types/metadata_stream_from_map.h>
-
-#include <tests/test_gtest.h>
-
-using namespace kwiver::vital;
-
-namespace {
-
-// ----------------------------------------------------------------------------
-void
-test_istream_frame(
-  metadata_istream& is, frame_id_t frame, metadata_vector const& md )
-{
-  ASSERT_FALSE( is.at_end() );
-  EXPECT_EQ( frame, is.frame_number() );
-  EXPECT_EQ( md, is.metadata() );
-}
-
-// ----------------------------------------------------------------------------
-void
-test_istream_at_end( metadata_istream& is )
-{
-  // Repeat in case next_frame() changes state
-  for( size_t i = 0; i < 2; ++i )
-  {
-    ASSERT_TRUE( is.at_end() );
-    EXPECT_THROW( is.frame_number(), std::invalid_argument );
-    EXPECT_THROW( is.metadata(), std::invalid_argument );
-    EXPECT_FALSE( is.next_frame() );
-  }
-}
-
-// ----------------------------------------------------------------------------
-void
-test_ostream_frame(
-  metadata_ostream& os, frame_id_t frame, metadata_vector const& md )
-{
-  ASSERT_FALSE( os.at_end() );
-  ASSERT_NO_THROW( ASSERT_TRUE( os.write_frame( frame, md ) ) );
-}
-
-// ----------------------------------------------------------------------------
-void
-test_ostream_at_end( metadata_ostream& os )
-{
-  ASSERT_TRUE( os.at_end() );
-  EXPECT_THROW( os.write_frame( 1024, {} ), std::invalid_argument );
-  ASSERT_TRUE( os.at_end() );
-  EXPECT_NO_THROW( os.write_end() );
-  ASSERT_TRUE( os.at_end() );
-}
-
-} // namespace <anonymous>
 
 // ----------------------------------------------------------------------------
 int
