d717543bafaa8ca7089dcfa4e25f08f33a99b9f6
Roddy Collins <roddy.collins@kitware.com>
2024-11-17 11:16:32 -0500

    Tweak stereo CLI to accept camera, left-right points as parameters

--- examples/stereo/main.cpp
+++ examples/stereo/main.cpp
@@ -2,23 +2,32 @@
 #include <arrows/mvg/triangulate.h>
 #include <vital/io/camera_rig_io.h>
 
+using std::cout;
+using std::cerr;
+using std::atof;
+
 int main(int argc, char *argv[])
 {
-  std::cout << "Hello world\n";
+  if (argc != 6)
+  {
+    cerr << "Usage: " << argv[0] << " camfile leftX leftY rightX rightY\n";
+    return -1;
+  }
 
-  Eigen::Matrix<float, 2, 1> pt;
-  pt << 320,  240;
-  std::cout << "Input point: " << pt << "\n";
-
-  auto stereo_rig_ptr = kwiver::vital::read_stereo_rig( "/Users/roddy/work/doe-windfarm/t001-2024-09-bootup/data/Calib_Results_stereo_SH18_12_rectified.json" );
-  std::cout << "left: " << stereo_rig_ptr->left() << "\n";
-  std::cout << "right: " << stereo_rig_ptr->right() << "\n";
+  auto stereo_rig_ptr = kwiver::vital::read_stereo_rig( argv[1] );
+  cout << "left: " << stereo_rig_ptr->left() << "\n";
+  cout << "right: " << stereo_rig_ptr->right() << "\n";
 
   kwiver::vital::simple_camera_perspective& left_cam(dynamic_cast<kwiver::vital::simple_camera_perspective&>(* (stereo_rig_ptr->left())));
   kwiver::vital::simple_camera_perspective& right_cam(dynamic_cast<kwiver::vital::simple_camera_perspective&>(* (stereo_rig_ptr->right())));
-  std::cout << "left camera:\n" << left_cam << "\n";
-  std::cout << "right camera:\n" << right_cam << "\n";
-  auto out_pt = kwiver::arrows::mvg::triangulate_fast_two_view(left_cam, right_cam, pt, pt);
-  std::cout << "Output point: " << out_pt << "\n";
+  cout << "left camera:\n" << left_cam << "\n";
+  cout << "right camera:\n" << right_cam << "\n";
+
+  Eigen::Matrix<float, 2, 1> leftPt(atof(argv[2]), atof(argv[3])), rightPt(atof(argv[4]), atof(argv[5]));
+
+  auto out_pt = kwiver::arrows::mvg::triangulate_fast_two_view(left_cam, right_cam, leftPt, rightPt);
+  cout << "Left point: " << leftPt << "\n";
+  cout << "Right point: " << rightPt << "\n";
+  cout << "Output point:\n" << out_pt << "\n";
 }
 
