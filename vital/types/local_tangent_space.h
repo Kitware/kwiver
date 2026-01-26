fe346b294e68b7a5948a92d5f396d9cf5433897d
Daniel Riehm <daniel.riehm@kitware.com>
2026-01-26 10:44:02 -0600

    Add utilities to local_tangent_space

--- vital/types/local_tangent_space.h
+++ vital/types/local_tangent_space.h
@@ -6,6 +6,7 @@
 #define VITAL_TYPES_LOCAL_TANGENT_SPACE_H_
 
 #include <vital/types/geo_point.h>
+#include <vital/types/rotation.h>
 #include <vital/types/vital_types_export.h>
 
 namespace kwiver {
@@ -24,11 +25,19 @@ namespace vital {
 ///      tangent to the earth's surface at the origin and the Z axis points up.
 ///   5. If the origin is not along the polar axis, the X axis points east and
 ///      the Y axis points north.
+///
+/// These guarantees only hold if the ``local_tangent_space`` has been
+/// initialized with a non-empty origin point.
 struct VITAL_TYPES_EXPORT local_tangent_space
 {
 public:
+  local_tangent_space();
+
   /// \param origin The point at which local coordinates will be zero.
   explicit local_tangent_space( geo_point const& origin );
+
+  /// Return \c true if this object holds a valid tangent space.
+  bool valid() const;
 
   /// Return the origin of the coordinate system.
   geo_point const& origin() const;
@@ -38,15 +47,46 @@ public:
   /// \param global_point Point in geographic coordinates to convert.
   vector_3d to_local( geo_point const& global_point ) const;
 
+  /// Convert \p global_rotation to the local coordinate system.
+  ///
+  /// \param global_rotation
+  ///   Point in geographic coordinates to convert, in the reference system
+  ///   defined by \p global_point.crs(). If that reference system is geodetic,
+  ///   East-North-Up axes are assumed.
+  /// \param global_point Point where the rotation is centered.
+  rotation_d to_local(
+    rotation_d const& global_rotation, geo_point const& global_point ) const;
+
   /// Convert \p local_point to geographic coordinates.
   ///
   /// \param local_point Point in local coordinates to convert.
   geo_point to_global( vector_3d const& local_point ) const;
 
+  /// Convert \p global_rotation to the local coordinate system.
+  ///
+  /// \param local_rotation
+  ///   Point in local coordinates to convert. Will be converted the reference
+  ///   system defined by \p global_point.crs(). If that reference system is
+  ///   geodetic, East-North-Up axes will be used.
+  /// \param global_point Point where the rotation is centered.
+  rotation_d to_global(
+    rotation_d const& local_rotation, geo_point const& global_point ) const;
+
 private:
-  geo_point const m_origin;
+  geo_point m_origin;
   Eigen::Matrix3d m_axes;
 };
+
+// ----------------------------------------------------------------------------
+VITAL_TYPES_EXPORT
+local_tangent_space
+read_local_tangent_space_from_file( std::string const& filepath );
+
+// ----------------------------------------------------------------------------
+VITAL_TYPES_EXPORT
+void
+write_local_tangent_space_to_file(
+  local_tangent_space const& local_space, std::string const& filepath );
 
 } // namespace vital
 
