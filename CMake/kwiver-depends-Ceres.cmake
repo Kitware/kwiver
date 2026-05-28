4ff456412a9bc1df5c454173ea3c3a86969c70a8
Daniel Riehm <daniel.riehm@kitware.com>
2026-05-28 10:28:26 -0500

    Compensate for missing CERES_INCLUDE_DIRS in Ceres v2

--- CMake/kwiver-depends-Ceres.cmake
+++ CMake/kwiver-depends-Ceres.cmake
@@ -7,5 +7,9 @@ option( KWIVER_ENABLE_CERES
 
 if( KWIVER_ENABLE_CERES )
   find_package( Ceres REQUIRED )
+  if(NOT CERES_INCLUDE_DIRS)
+    # Ceres v2 doesn't define CERES_INCLUDE_DIRS, but the ceres algo plugin still relies on it
+    get_target_property(CERES_INCLUDE_DIRS Ceres::ceres INTERFACE_INCLUDE_DIRECTORIES)
+  endif()
   include_directories( SYSTEM ${CERES_INCLUDE_DIRS} )
 endif()
