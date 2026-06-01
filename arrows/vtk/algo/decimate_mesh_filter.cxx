7acda22de378652002c043bab94edba9ab435175
Daniel <daniel.lichy@kitware.com>
2026-06-01 16:25:00 -0400

    Add mesh_filter algo and vtk implementations

---
+++ arrows/vtk/algo/decimate_mesh_filter.cxx
@@ -0,0 +1,69 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+/// \file
+/// Implementation of vtkDecimatePro mesh filtering algorithm
+
+#include <arrows/vtk/algo/decimate_mesh_filter.h>
+#include <arrows/vtk/algo/triangulate_mesh_filter.h>
+#include <arrows/vtk/mesh_container.h>
+#include <arrows/vtk/mesh_utils.h>
+#include <vital/exceptions.h>
+#include <vtkDecimatePro.h>
+
+namespace kwiver {
+
+namespace arrows {
+
+namespace vtk {
+
+void
+decimate_mesh_filter
+::initialize()
+{
+  attach_logger( "arrows.vtk.decimate_mesh_filter" );
+}
+
+decimate_mesh_filter::
+~decimate_mesh_filter() {}
+
+vital::mesh_container_sptr
+decimate_mesh_filter
+::filter( vital::mesh_container_sptr input )
+{
+  if( !input )
+  {
+    VITAL_THROW( vital::invalid_value, "Null input mesh" );
+  }
+
+  // vtkDecimatePro requires triangular input
+  triangulate_mesh_filter triangulator;
+  vtkSmartPointer< vtkPolyData > mesh =
+    container_to_polydata( *triangulator.filter( input ) );
+
+  auto decimator = vtkSmartPointer< vtkDecimatePro >::New();
+  decimator->SetInputData( mesh );
+  decimator->SetTargetReduction( c_target_reduction );
+  decimator->SetFeatureAngle( c_feature_angle );
+  decimator->SetPreserveTopology( c_preserve_topology );
+  decimator->SetMaximumError( c_maximum_error );
+  decimator->SetAbsoluteError( c_absolute_error );
+  decimator->SetErrorIsAbsolute( c_error_is_absolute );
+  decimator->SetAccumulateError( c_accumulate_error );
+  decimator->SetSplitAngle( c_split_angle );
+  decimator->SetSplitting( c_splitting );
+  decimator->SetPreSplitMesh( c_pre_split_mesh );
+  decimator->SetDegree( c_degree );
+  decimator->SetBoundaryVertexDeletion( c_boundary_vertex_deletion );
+  decimator->SetInflectionPointRatio( c_inflection_point_ratio );
+  decimator->Update();
+
+  return std::make_shared< mesh_container >( decimator->GetOutput() );
+}
+
+} // namespace vtk
+
+} // namespace arrows
+
+} // namespace kwiver
---
+++ vital/algo/mesh_filter.h
@@ -0,0 +1,50 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+/// \file
+/// Interface to abstract filter mesh algorithm
+
+#ifndef VITAL_ALGO_MESH_FILTER_H
+#define VITAL_ALGO_MESH_FILTER_H
+
+#include <vital/algo/algorithm.h>
+#include <vital/vital_config.h>
+
+#include <vital/types/mesh_container.h>
+
+namespace kwiver {
+
+namespace vital {
+
+namespace algo {
+
+/// \brief Abstract base class for mesh filter algorithms.
+class VITAL_ALGO_EXPORT mesh_filter
+  : public kwiver::vital::algorithm
+{
+public:
+  mesh_filter();
+  PLUGGABLE_INTERFACE( mesh_filter );
+
+  /// Filter an input mesh and return resulting mesh.
+  ///
+  /// This method implements the filtering operation. The method does
+  /// not modify the mesh in place. The resulting mesh must be a
+  /// newly allocated mesh.
+  ///
+  /// \param mesh_data Mesh to filter.
+  /// \returns a filtered version of the input mesh.
+  virtual mesh_container_sptr
+  filter( mesh_container_sptr mesh_data ) = 0;
+};
+
+typedef std::shared_ptr< mesh_filter > mesh_filter_sptr;
+
+} // namespace algo
+
+} // namespace vital
+
+} // namespace kwiver
+
+#endif
