6ff2200d5d342122c3a0dab7fd42acd67a1327cf
Matt Dawkins <matt.dawkins@kitware.com>
2026-06-29 00:57:07 -0400

    Add missing <cstdint> includes for GCC 13 / CUDA 13 builds
    GCC 13's libstdc++ no longer transitively includes <cstdint>, so files
    using uint64_t without including it fail to compile with
    'uint64_t does not name a type'. Add the include where needed.

--- track_oracle/file_formats/track_kw18/track_kw18_instances.cxx
+++ track_oracle/file_formats/track_kw18/track_kw18_instances.cxx
@@ -2,6 +2,8 @@
 // OSI-approved BSD 3-Clause License. See top-level LICENSE file or
 // https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
 
+#include <cstdint>
+
 #include <vgl/vgl_point_2d.h>
 #include <vgl/vgl_box_2d.h>
 
