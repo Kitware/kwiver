5f84749d15fe28919b33255007b94485658379e1
Matt Dawkins <matt.dawkins@kitware.com>
2026-07-10 09:01:36 -0400

    Make Boost an optional dependency
    Sprokit no longer needs Boost: boost::thread, boost::graph and the remaining
    boost utilities were replaced with standard library equivalents, and the only
    surviving includes are vendored headers that guard on their own macros.  Drop
    the REQUIRED from find_package(Boost) and guard the BOOST_ROOT plumbing and the
    few link lines that referenced Boost_* variables on Boost_FOUND.

--- CMake/kwiver-depends-Boost.cmake
+++ CMake/kwiver-depends-Boost.cmake
@@ -1,9 +1,9 @@
-# Required Boost external dependency
+# Optional Boost external dependency
 if (NOT DEFINED KWIVER_BOOST_VERSION)
   set(KWIVER_BOOST_VERSION 1.54)
 endif()
 
-find_package(Boost ${KWIVER_BOOST_VERSION} REQUIRED
+find_package(Boost ${KWIVER_BOOST_VERSION}
   COMPONENTS
   chrono
   date_time
@@ -14,12 +14,14 @@ find_package(Boost ${KWIVER_BOOST_VERSION} REQUIRED
   system
   thread)
 
-add_definitions(-DBOOST_ALL_NO_LIB)
+if(Boost_FOUND)
+  add_definitions(-DBOOST_ALL_NO_LIB)
 
-if(WIN32)
-  add_definitions(-DBOOST_PROGRAM_OPTIONS_DYN_LINK)
+  if(WIN32)
+    add_definitions(-DBOOST_PROGRAM_OPTIONS_DYN_LINK)
+  endif()
+
+  include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
+  link_directories(${Boost_LIBRARY_DIRS})
 endif()
-
-include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
-link_directories(${Boost_LIBRARY_DIRS})
 ####
